3a149ec6214d1a6000e8eab6c7d6defa0f3488f1
Alex Klick <aklick12@gmail.com>
2021-01-06 19:20:04 -0600

    updated fizzbuzz

--- day_7/caesar_cipher.rb
+++ day_7/caesar_cipher.rb
@@ -6,21 +6,22 @@ class CeasarCipher
     end
     def get_shifty
         index  = 0
-        str_len = 26
-        letters = {'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7,'i':8,'j':9,'k':10,'l':11,'m':12,'n':13,'o':14,'p':15,'q':16,'r':17,'s':18,'t':19,'u':20,'v':21,'w':22,'x':23,'y':24,'z':25}
+        str_len = 25
+        alphabet = {'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7,'i':8,'j':9,'k':10,'l':11,'m':12,'n':13,'o':14,'p':15,'q':16,'r':17,'s':18,'t':19,'u':20,'v':21,'w':22,'x':23,'y':24,'z':25}
         replacement = Array.new(26)
+        #sets the new hash alphabet 
         while index < str_len
-            if index + shift >= str_len -1
-                replacement[index + shift - str_len] = letters.key(index)
+            if index + shift >= str_len 
+                replacement[index + shift - str_len] = alphabet.key(index)
             elsif (index + shift) < (str_len -1)
-                replacement[index + shift ] = letters.key(index)
+                replacement[index + shift ] = alphabet.key(index)
             end
             index += 1
         end
         index = 0
         while index < string.length
             if string[index] != " "
-                string[index] = replacement[letters.values_at(string[index].to_sym)[0]].to_s
+                string[index] = replacement[alphabet.values_at(string[index].to_sym)[0]].to_s
             end
             index += 1
         end
@@ -29,7 +30,7 @@ class CeasarCipher
     end
 end
 def run_caeser
-    p 'Please set the shift quantity' 
+    p 'Please set the shift value' 
     shift = gets.chomp.to_i
     p 'Thanks! Now please enter a string'
     string = gets.chomp.split('')
--- day_7/fizzbuzz.rb
+++ day_7/fizzbuzz.rb
@@ -1,6 +1,6 @@
 def print_rules
     number = 1
-    while number < 100
+    while number < 101
         if (number % 5 == 0) && (number % 3 == 0)
             p 'FizzBuzz'
         elsif number % 5 == 0
@@ -14,3 +14,23 @@ def print_rules
     end
 end
 print_rules
+
+def print_rules_unlimited
+    p 'Set a Number:'
+    input = gets.chomp.to_i
+    number = 1
+    while number < input + 1
+        if (number % 5 == 0) && (number % 3 == 0)
+            p 'FizzBuzz'
+        elsif number % 5 == 0
+            p 'Buzz'
+        elsif number % 3 == 0
+            p 'Fizz'
+        else
+            p number
+        end
+        number += 1
+    end
+end
+
+print_rules_unlimited

6ada5c13f450a1acdbe39d356afa5bee6b552fe7
Aidan McGuire <>
2021-01-11 21:09:12 -0700

    Edit typos and format

--- section3/exercises/hash_practice.rb
+++ section3/exercises/hash_practice.rb
@@ -1,7 +1,9 @@
 grades = {"aidan" => 10, "jess" => 10, "grace" => 9}
 grades.default = "Does not exist"
 puts grades[10]
-#puts grades["aidan"]
+puts "-" * 10
+puts grades["aidan"]
+puts "-" * 10
 grades.each do |student, grade|
   puts "student: #{student} grade:#{grade}"
 end

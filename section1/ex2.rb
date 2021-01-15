f7d698167de4c7c0fe820ba17626c8d57f574d75
Gunnar Runkle <dunkcrunk@gmail.com>
2021-01-14 18:31:36 -0700

    Created commetns in code

---
+++ section1/ex2.rb
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your program later.
+# Antyhing after the # is ignored by ruby.
+
+puts "I could code like this" # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# puts "this won't run."
+
+puts "This will run"

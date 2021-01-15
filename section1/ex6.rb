d511bd7ef13ca159b91e44a1102e7979a31a1ba2
Gunnar Runkle <dunkcrunk@gmail.com>
2021-01-14 21:23:41 -0700

    Created more strings

---
+++ section1/ex6.rb
@@ -0,0 +1,35 @@
+#setting type to a numnber
+types_of_people = 10
+#setting the variable x to a bunch of words including types_of_people
+x = "There are #{types_of_people} types of people."
+# setting binary to binary
+binary = "binary"
+# setting do_not to say don't
+do_not = "don't"
+#setting y to a string of words including the binary and do_not which are set to a value
+y = "Those who know #{binary} and those who #{do_not}."
+# displays x  which was defined earlier
+puts x
+# displays y  which was defined earlier
+puts y
+
+#displays the words with a x variable defined earlier
+puts "I said: #{x}."
+#displays the words with a y variable defined earlier
+puts "I also said: '#{y}'."
+
+#setting hilarious to false
+hilarious = false
+#setting joke_evaluation to a string of words
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+#displasy joke_evaluation which is identified earlier
+puts joke_evaluation
+
+#setting w to a string of words
+w = "This is the left side of..."
+##setting e to a string of words
+e = "a string with a right side."
+
+#displasy the string w represents and adds the string e represents
+puts w + e

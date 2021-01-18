d9b3289f233aead925edba6ed44e66da30463358
Gunnar Runkle <dunkcrunk@gmail.com>
2021-01-18 02:34:33 -0700

    Finished final project

---
+++ finalProject/10_speckled_frogs.rb
@@ -0,0 +1,17 @@
+x = 11
+y = 2
+
+until x < y do
+  x -= 1;
+if
+x > 1
+  puts "#{x} speckled frogs sat on a log eating some most delicious bugs.
+One jumped in the pool where its nice and cool, then there were #{x-1} speckled frogs."
+end
+if
+  x == 1
+  puts "#{x} speckled frog sat on a log eating some most delicious bugs.
+One jumped in the pool where its nice and cool,
+then there were no more speckled frogs!"
+end
+end

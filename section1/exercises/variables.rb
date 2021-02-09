05fa2b965e3511d2f00122e719428fc71be0671e
Caleb Wittman <calebjwittman1991@gmail.com>
2021-02-09 09:16:48 -0700

    Add Section 1

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
@@ -11,49 +11,61 @@ p name
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+p house_elf
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+hp = "Harry potter must not return to Hogwarts!"
+p hp
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
-# YOUR CODE HERE
-p students
+stadd = "#{students + 2}"
+p stadd
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
-# YOUR CODE HERE
-p students
+stsub = "#{students - 2}"
+p stsub
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
-
+first_name = "Caleb"
+is_hungry = "yes"
+number_of_pets = 4
+p "My name is #{first_name} and am I hungry? #{is_hungry.upcase}! I also have #{number_of_pets} pets!"
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
+# How did you decide to use the data type you did for each of the three variables above?
 
 # Explain.
-
+# I determined the data type by what made the most sense. Each seems to be prompting a question, i.e. what is your first name
+# how many pets do you have? Using the information from there, I determined is hungry was asking whether or not I was hungry, and so I answered the question.
+# it could also be suggested that we used an interger, a boolean (sort of with is hungry) and a regular string with first name
 
 # YOU DO:
 # Re-assign the values to the three variables from the previous challenge to different values (but same data type).
 # print all three variables to the terminal.
+first_name = "David"
+is_hungry = "no"
+number_of_pets = 7
+
+p "This is #{first_name} and is he hungry? #{is_hungry}. He has #{number_of_pets} pets!"
 
 
 # YOU DO:
 # Using the variables below, print the total number of snacks to the terminal:
 healthy_snacks = 6;
 junk_food_snacks = 8;
+p "There are a total of #{healthy_snacks + junk_food_snacks} snacks."
 
 
 #-------------------
 # FINAL CHECK
 #-------------------
 
-# Did you run this file in your terminal to make sure everything printed out to the terminal 
-  # as you would expect? 
+# Did you run this file in your terminal to make sure everything printed out to the terminal
+  # as you would expect?
--- section1/reflection.md
+++ section1/reflection.md
@@ -2,18 +2,43 @@
 
 1. How did the SuperLearner Article resonate with you? What from this list do you already do? Want to start doing or do more of? Is there anything not on this list, that you would add to it?
 
+The Superlearner Article articulated a swath of fantastic strategizes and characteristics essential to being a continuing learner. While, at times, it did feel like a "Forbes' five things person A does every morning" article, I still think it was successful in its intent. The ideas of taking care of the brain and taking short breaks every ~50 minutes are things I would love to implement, as well as reading more. I currently continue to believe I can learn and unlearn and relearn, but I can always be better at it. I wouldn't add anything to the article, I think it exhibits content the average reader wouldn't often think of.
+
 1. How would you print the string `"Hello World!"` to the terminal?
+
+```p "Hello World!""
+```
 
 1. What character is used to indicate comments in a ruby file?
 
+A pound sign "#"
+
 1. Explain the difference between an integer and a float?
+
+An integer and float are both numbers, however a float contains a decimal whereas an integer does not. This makes a float more accurate.
 
 1. In the space below, create a variable `animal` that holds the string `"zebra"`
 
+```animal = "zebra"
+```
+
 1. How would you print the string `"zebra"` using the variable that you created above?
+
+```p animal
+```
 
 1. What is interpolation? Use interpolation to print a sentence using the variable `animal`.
 
+Interpolation is putting a variable or variables inside of string. For example:
+
+```p "The #{animal} has fantastic stripes!"
+```
+
 1. What method is used to get input from a user?
 
+```gets.chomp
+```
+
 1. Name and describe two common string methods:
+
+.upcase will capitalize every part of the attached string, .chomp removes the /n.

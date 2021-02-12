3e4d69a6d87857a780913a79514cf6ec4ca3a55b
Lawrence Whalen <ldwhalen@gmail.com>
2021-02-12 16:19:56 -0700

    Follow example one

---
+++ section1/ex1.rb
@@ -0,0 +1,9 @@
+puts "Hellow World!"
+puts "Hellow Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "Tellis is fun."
+# puts won't print cause this is comment.

7504fdeeec114c417fbad5bf0fab277cb30a68ce
Noah Zinter <noah.zinter@gmail.com>
2021-02-13 14:05:26 -0700

    Complete strings.rb

--- section1/exercises/strings.rb
+++ section1/exercises/strings.rb
@@ -5,11 +5,21 @@
 
 # Example: Write code that prints your name to the terminal:
 p "Alan Turing"
+puts "Noah Zinter"
 
 # Write code that prints `Welcome to Turing!` to the terminal:
-p #YOUR CODE HERE
+puts "Welcome to Turing!"#YOUR CODE HERE
 
 # Write code that prints `99 bottles of pop on the wall...` to the terminal:
-# YOUR CODE HERE
+puts "99 bottles of pop on the wall..."# YOUR CODE HERE
 
 # Write out code to log one line from your favorite song or movie.
+puts """
+What discordant vespers do the tinkers goods chime
+through the long twilight and over the brindled forest road?
+Him stooped and hounded through the last windy recrements of day
+like those old exiles who divorced of corporeality and
+enjoined ingress of heaven or hell
+wander forever the middle warren
+spoorless increate and anathema.
+"""

fa80abfaab3d8857318dec4a51d816df044c433d
Lawrence Whalen <ldwhalen@gmail.com>
2021-02-12 18:01:36 -0700

    Finish study drills for ex5

--- section1/ex5.rb
+++ section1/ex5.rb
@@ -1,17 +1,19 @@
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie in 2009
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown' # not a lie in 2009
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown' # not a lie in 2009
+centimeters_convert = 2.54
+kilograms_convert = 0.453592
 
-puts "Let's talk about #{my_name}."
-puts "He's #{my_height * 2.54} centimeters tall."
-puts "He's #{my_weight *0.453592} kilograms heavy"
+puts "Let's talk about #{name}."
+puts "He's #{height * centimeters_convert} centimeters tall."
+puts "He's #{weight * kilograms_convert} kilograms heavy"
 puts "Actually that's not to heavy."
-puts "He's got #{my_eyes} eyes and #{my_hair} hair."
-puts "His teeth are usually #{my_teeth} depending on the coffee."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
 
 # this line is tricky, try to get it exactly right
-puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."

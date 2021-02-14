169e28fa75589d1f9176c38c16efc2566af92e51
Lawrence Whalen <ldwhalen@gmail.com>
2021-02-13 21:11:41 -0700

    Complete arrays exercises

--- section3/exercises/arrays.rb
+++ section3/exercises/arrays.rb
@@ -23,42 +23,49 @@ print animals[0]
 
 # YOU DO: Write code below that will print the number of elements in array of
 # animals from above.
-
+p animals.length
 
 # YOU DO: Write code that will reassign the last item in the animals
 # array to "Gorilla"
-
-
+animals[-1] = "Gorilla"
+p animals
 # YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
-
+animals[3] = "Duck"
+p animals
 # YOU DO: Write code that will print the String "Elephant" in the animals array
-
+p animals.append("Elephant")
 
 #-------------------
 # PART 2: Foods: Array Methods
 #-------------------
 
 # YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+food_array = ["grape", "potato", "cheese", "nuts"]
+p food_array
 
 # YOU DO: Write code below that will print the number of elements in the array of
 # foods from above.
-
+p food_array.length
 
 # YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 # print the changed array to verify "broccoli" has been added
-
+food_array.append("broccoli")
+p food_array
 
 # YOU DO: Write code below that removes the last item of food from the foods array and
 # print the changed array to verify that item has been removed
+food_array.pop
+p food_array
 
-
-# YOU DO: Write code to add 3 new foods to the array. 
+# YOU DO: Write code to add 3 new foods to the array.
   # There are several ways to do this - choose whichever you'd like!
 # Then, print the changed array to verify the new items have been added
+food_array.append("peas", "steak", "tomato")
+p food_array
 
 # YOU DO: Remove the food that is in index position 0.
+food_array.delete_at(0)
+p food_array
 
 #-------------------
 # PART 3: Where are Arrays used?
@@ -77,11 +84,9 @@ print animals[0]
 posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 # YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-# may be storing data? Come up with 3 examples - they could be from different web applications or 
+# may be storing data? Come up with 3 examples - they could be from different web applications or
 # all from the same one.
 
-# 1:
-# 2:
-# 3:
-
-
+reddit_favorites = ['aww', 'ruby', 'kittens', 'git']
+gmail_storage = ['inbox', 'trash', 'spam', 'archive']
+github_profile = ['profile', 'repositories', 'projects', 'stars', 'gists']

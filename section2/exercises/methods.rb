f3e09e833e1483dab1d24ee3fd3454edd7bc4459
Lawrence Whalen <ldwhalen@gmail.com>
2021-02-13 18:09:21 -0700

    Complete methods exercise

--- section2/exercises/methods.rb
+++ section2/exercises/methods.rb
@@ -12,20 +12,35 @@ print_name
 
 # Write a method that takes a name as an argument and prints it:
 def print_name(name)
-  # YOUR CODE HERE
+  p name
 end
 
 print_name("Albus Dumbledore")
 
-# Write a method that takes in 2 numbers as arguments and prints 
+# Write a method that takes in 2 numbers as arguments and prints
 # their sum. Then call your method three times with different arguments passed in:
-# YOUR CODE HERE
+def addition(num1, num2)
+  p num1 + num2
+end
 
-# Write a method that takes in two strings as arguments and prints 
-# a concatenation of those two strings. Example: The arguments could be 
-# (man, woman) and the end result might output: "When Harry Met Sally".  
-# Then call your method three times with different arguments passed in. 
+addition(1,2)
+p = 5
+s = 6
+addition(p, s)
+addition(5 / 6, 13 * 2)
+# Write a method that takes in two strings as arguments and prints
+# a concatenation of those two strings. Example: The arguments could be
+# (man, woman) and the end result might output: "When Harry Met Sally".
+# Then call your method three times with different arguments passed in.
+def two_food_sentence(food1, food2)
+  p "I love to eat #{food1}, and I hate when I smell #{food2}!"
+end
 
+two_food_sentence("eggs", "roadkill")
+food1 = "tofu"
+food2 = "rice"
+two_food_sentence(food1, food2)
+two_food_sentence(food1, "hamburgers")
 
 #-------------------
 # PART 3: Naming is Hard
@@ -37,5 +52,5 @@ print_name("Albus Dumbledore")
 # Look at the code you wrote for the previous YOU DO🎈 - what did you name the function, and why?
 # What did you name each parameter, and why?
 
-# EXPLAIN:
-
+# EXPLAIN: I named the method two_food_sentence because it would output a sentence containing two foods.
+#I named the parameters food1 and food2 because in this hypothetical program we passed the method two foods generated elswhere and those names kept them easily identified.

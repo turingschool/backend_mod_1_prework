ea5eed6a11e4b88226cd194f8cb927114f7deddd
Richard DeSilvey <rdesilvey@gmail.com>
2021-02-16 14:29:23 -0700

    Implemented the class

--- section4/exercises/person.rb
+++ section4/exercises/person.rb
@@ -1,5 +1,25 @@
-# Create a person class with at least 2 attributes and 2 behaviors. 
+# Create a person class with at least 2 attributes and 2 behaviors.
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
 # YOUR CODE HERE
+
+class Person
+  def initialize (name, age)
+    @name = name
+    @age = age
+  end
+
+  def say_intro
+    puts "Hi! My name is #{@name} and I'm #{@age} years old."
+  end
+
+  def have_a_birthday
+    @age += 1
+    puts "I'm now #{@age} years old!"
+end
+
+person = Person.new("Richard", 33)
+
+person.say_intro
+person.have_a_birthday

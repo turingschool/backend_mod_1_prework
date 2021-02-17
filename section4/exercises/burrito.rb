992c8127e663bed7655830b9ed9e3d40e6650144
Zach Green <zachjamesgreen@gmail.com>
2021-02-16 20:45:01 -0700

    Added section 4

--- section4/exercises/burrito.rb
+++ section4/exercises/burrito.rb
@@ -1,4 +1,4 @@
-# Add the following methods to this burrito class and 
+# Add the following methods to this burrito class and
 # call the methods below the class:
 # 1. add_topping
 # 2. remove_topping
@@ -13,17 +13,27 @@ class Burrito
   end
 
   def add_topping(topping)
-    self.toppings.push(topping)
+    @toppings.push(topping)
   end
 
-  def remove_topping()
+  def remove_topping(topping)
+    @toppings.delete(topping)
   end
 
-  def change_protein()
+  def change_protein(protein)
+    @protein = protein
   end
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
+p dinner.protein
+p dinner.base
+p dinner.toppings
+
+dinner.add_topping("sour cream")
+dinner.remove_topping("cheese")
+dinner.change_protein("carne")
+
 p dinner.protein
 p dinner.base
 p dinner.toppings

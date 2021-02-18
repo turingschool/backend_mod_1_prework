0150f9b2a2fe767f5a2b19ad8003553f99806a94
Mitch Standley <mitch_standley@yahoo.com>
2021-02-18 13:35:46 -0600

    Semi complete annotations

--- final_prep/annotations.rb
+++ final_prep/annotations.rb
@@ -4,11 +4,18 @@
 
 # Build a Bear
 
+# declare a function build_a_bear, it takes 5 parameters.
 def build_a_bear(name, age, fur, clothes, special_power)
+# variable called greeting assigned to a string interpolation with name parameter.
   greeting = "Hey partner! My name is #{name} - will you be my friend?!"
+# variable called demographics assigned to an array containing 2 items including name and age parameters.
   demographics = [name, age]
+# variable assigned to string interpolated with special_power parameter.
   power_saying = "Did you know that I can #{special_power}?"
+
+# variable assigned to a hash
   built_bear = {
+# declared a variable and assigned it to a hash with key/values following.
     'basic_info' => demographics,
     'clothes' => clothes,
     'exterior' => fur,
@@ -16,15 +23,17 @@ def build_a_bear(name, age, fur, clothes, special_power)
     'sayings' => [greeting, power_saying, "Goodnight my friend!"],
     'is_cuddly' => true,
   }
+#
   return built_bear
 end
 
 build_a_bear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares')
+#Created
 build_a_bear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in')
 
 
 # FizzBuzz
-
+# declare a function fizzbuzz, it takes three parameters.
 def fizzbuzz(num_1, num_2, range)
   (1..range).each do |i|
     if i % num_1 === 0 && i % num_2 === 0

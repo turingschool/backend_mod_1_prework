b05a02fbe5ab339c7bee66fcaadea18a1ea0bb69
deebot10 <dee1jhill@gmail.com>
2021-02-24 23:41:44 -0700

    Add Section 1

--- section1/exercises/strings.rb
+++ section1/exercises/strings.rb
@@ -4,12 +4,15 @@
 # `ruby section1/exercises/strings.rb`
 
 # Example: Write code that prints your name to the terminal:
-p "Alan Turing"
+puts "dee"
 
 # Write code that prints `Welcome to Turing!` to the terminal:
-p #YOUR CODE HERE
+puts "Welcome to Turing!"
 
 # Write code that prints `99 bottles of pop on the wall...` to the terminal:
-# YOUR CODE HERE
+puts "99 bottles of pop on the wall..."
 
 # Write out code to log one line from your favorite song or movie.
+puts "Tell me your favorite quote for a song or movie"
+quote = gets.chomp
+puts quote

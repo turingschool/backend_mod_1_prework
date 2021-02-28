ca84b4f5619d2410f8f5514d1d70dae155657da3
Suzie K <suziekim.dev@gmail.com>
2021-02-28 10:57:06 -0500

    Complete hashes.rb exercises

--- section3/exercises/hashes.rb
+++ section3/exercises/hashes.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section3/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
@@ -8,25 +8,39 @@ foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+zoo = {
+  'penguins' => 12,
+  'elephants'=> 10,
+  'lizards' => 5,
+  'bears' => 8
+}
+
 p zoo
 
-# Write code that prints all of the 'keys' of the zoo variable 
+# Write code that prints all of the 'keys' of the zoo variable
 # you created above:
 # YOUR CODE HERE
 
-# Write code that prints all of the 'values' of the zoo variable 
+puts zoo.keys
+
+# Write code that prints all of the 'values' of the zoo variable
 # you created above:
 # YOUR CODE HERE
 
-# Write code that prints the value of the first animal of the zoo variable 
+puts zoo.values
+
+# Write code that prints the value of the first animal of the zoo variable
 # you created above:
 # YOUR CODE HERE
 
-# Write code that adds an animal to the zoo hash. 
+puts zoo['penguins']
+
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
 # YOUR CODE HERE
+ zoo['lions'] = 5
 
+ puts zoo
 
 #-------------------
 # Part 2: Email
@@ -39,17 +53,27 @@ p zoo
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
+student_emails = {
+  'suzie@turing.edu' => 'Troubleshooting Github',
+  'mara@turing.edu' => 'Enrollment Fee Questions',
+  'jay@turing.edu' => 'Interview Times',
+  'ann@turing.edu' => 'Snow days?',
+  'bes@turing.edu' => 'Taking Sick Day'
+}
+
 # Write code that prints your email hash to the terminal.
+puts student_emails
 
+# Write code that prints all of the 'keys' of the email hash
+# you created above:
+# YOUR CODE HERE
+puts student_emails.keys
 
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'values' of the email hash
 # you created above:
 # YOUR CODE HERE
 
-# Write code that prints all of the 'values' of the email hash 
-# you created above:
-# YOUR CODE HERE
-
+puts student_emails.values
 
 #-------------------
 # Part 3: Many Emails - OPTIONAL EXTENSION
@@ -64,7 +88,7 @@ p zoo
 # posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 
-# Frankly, that was a very simplified version of the Array the Instagram developers have 
+# Frankly, that was a very simplified version of the Array the Instagram developers have
 # written and work with. Still probably slightly simplified as we don't know what their code
 # actually looks like, but it may look more like this:
 
@@ -76,7 +100,7 @@ posts = [
     'timestamp' => "4:37 PM August 13, 2019",
     'number_likes' => 0,
     'comments' => []
-  }, 
+  },
   {
     'image_src' => "./images/holiday-party.png",
     'caption' => "What a great holiday party omg",
@@ -90,12 +114,46 @@ puts posts
 puts posts[0]
 
 
-# The code snippet above shows an Array with 2 elements. Each element in an 
-# Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in an
+# Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same
 # key-value pairs you used in your email Object above.
 # Then, log the email Array to the console.
+student_emails = [
+  {
+    'name' => 'Suzie',
+    'email' => 'suzie@turing.edu',
+    'email_subject' => 'Troubleshooting Github',
+    'timestamp' => '11:20 PM'
+  },
+  {
+    'name' => 'Mara',
+    'email' => 'mara@turing.edu',
+    'email_subject' => 'Enrollment Fee Question',
+    'timestamp' => '3:16 PM'
+  },
+  {
+    'name' => 'Jay',
+    'email' => 'jay@turing.edu',
+    'email_subject' => 'Interview Times',
+    'timestamp' => '1:20 AM'
+  },
+  {
+    'name' => 'Ann',
+    'email' => 'ann@turing.edu',
+    'email_subject' => 'Snow days?',
+    'timestamp' => '9:38 AM'
+  },
+  {
+    'name' => 'Bes',
+    'email' => 'bes@turing.edu',
+    'email_subject' => 'Taking Sick Day',
+    'timestamp' => '6:23 AM'
+  }
+]
+
+puts student_emails

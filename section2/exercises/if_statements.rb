94a5f205cf0924521b7e55806ffb3a1f0fab7ed4
Sarah Rudy <sarahrudy@gmail.com>
2021-03-03 13:28:41 -0700

    Add Section 2 work

--- section2/exercises/if_statements.rb
+++ section2/exercises/if_statements.rb
@@ -3,7 +3,7 @@
 # file by entering the following command in your terminal:
 # `ruby section2/exercises/if_statements.rb`
 
-# Example: Using the weather variable below, write code that decides 
+# Example: Using the weather variable below, write code that decides
 # what you should take with you based on the following conditions:
   # if it is sunny, print "sunscreen"
   # if it is rainy, print "umbrella"
@@ -27,6 +27,21 @@
 # Experiment with manipulating the value held in variable 'weather'
 # to print something other than 'coat'
 
+weather = 'icy'
+
+if weather == 'sunny'
+  p "sunscreen"
+elsif weather == 'rainy'
+  p "umbrella"
+elsif weather == 'snowy'
+  p "coat"
+elsif weather == 'icy'
+  p "yak traks"
+else
+  p "good to go!"
+end
+
+# output will be "yak traks"
 
 ##################
 # Using the num_quarters variable defined below, determine
@@ -35,11 +50,8 @@
 
 # Right now, the program will print
 # out both "I have enough money for a gumball" and
-# "I don't have enough money for a gumball". Write a 
+# "I don't have enough money for a gumball". Write a
 # conditional statement that prints only one or the other.
-
-# Experiment with manipulating the value held within num_quarters
-# to make sure both conditions can be achieved.
 
 num_quarters = 0
 
@@ -47,9 +59,34 @@ puts "I have enough money for a gumball"
 puts "I don't have enough money for a gumball"
 
 
+num_quarters = 0
+  if num_quarters >= 2
+    puts "I have enough money for the gumball"
+  elsif num_quarters <= 2
+    puts "I don't have enough money for a gumball"
+  end
+
+# Experiment with manipulating the value held within num_quarters
+# to make sure both conditions can be achieved.
+
+num_quarters = 0
+  if num_quarters >= 2
+    puts "I have enough money for a gumball"
+  elsif num_quarters <= 2
+    puts "I don't have enough money for a gumball"
+  end
+
+num_quarters = 2
+  if num_quarters >= 2
+    puts "I have enough money for a gumball"
+  elsif num_quarters <= 2
+    puts "I don't have enough money for a gumball"
+  end
+
+
 #####################
 # Using the variables defined below, write code that will tell you
-# if you have the ingredients to make a pizza. A pizza requires 
+# if you have the ingredients to make a pizza. A pizza requires
 # at least two cups of flour and sauce.
 
 # You should be able to change the variables to achieve the following outputs:
@@ -63,3 +100,28 @@ puts "I don't have enough money for a gumball"
 
 cups_of_flour = 1
 has_sauce = true
+
+if cups_of_flour == 1 and has_sauce == true
+  puts "I cannot make pizza"
+end
+
+cups_of_flour = 5
+has_sauce = false
+
+if cups_of_flour == 5 and has_sauce == false
+  puts "I cannot make pizza"
+end
+
+cups_of_flour = 2
+has_sauce = true
+
+if cups_of_flour == 2 and has_sauce == true
+  puts "I can make pizza"
+end
+
+cups_of_flour = 3
+has_sauce = true
+
+if cups_of_flour == 3 and has_sauce == true
+  puts "I can make pizza"
+end
--- section2/reflection.md
+++ section2/reflection.md
@@ -2,28 +2,86 @@
 
 1. Regarding the blog posts in Part A, how do you feel about asking questions? Do you tend to ask them too soon, or wait too long, or somewhere in between?
 
+I've never been shy about asking questions. In my last job I was praised for being able to ask the right questions to push through bottle necks and get the team on the right track. It's been a different story for me in Mod 0 since everything is so brand new to me that I wasn't sure where to even start asking questions, but the more I learn the more confident I am in the material and the ability to ask good questions.
+
 ### If Statements
 
 1. What is a conditional statement? Give three examples.
 
+A conditional statement evaluates `true` or `false` and are used to add branching logic to your programs. They allow you to add complex behaviors that only occur under specific conditions.
+```
+3 < 8
+```
+```
+0 == 0
+```
+```
+5 > 3
+```
+
 1. Why might you want to use an if-statement?
+The expression after `if` is a condition. The condition is evaluated based on truth terms and if the conditon is true then the enclosed code will be run. If the condition is false, then the code will not be run.
 
 1. What is the Ruby syntax for an if statement?
+```
+people = 20
+cats = 30
+```
+```
+if people < cats
+  puts "Too many cats! The world is doomed!"
+end
+```
+output will be:
+
+```
+Too many cats! The world is doomed!"
+```
 
 1. How do you add multiple conditions to an if statement?
+You can add multiple conditions by including `if/elsif/else` structures.
 
 1. Provide an example of the Ruby syntax for an if/elsif/else statement:
+```
+x = 1
+if x > 2
+  puts "x is greater than 2"
+elsif x <= 2 an x!=0
+  puts "x is 1"
+else
+  puts "I can't guess the number"
+end
+```
+```
+x = 1
+```
 
 1. Other than an if-statement, can you think of any other ways we might want to use a conditional statement?
+
+If we want to make decisions, we might also want to use a conditional statement.
 
 ### Methods
 
 1. In your own words, what is the purpose of a method?
+We usually need to execute a piece of code several times in a program, so instead of typing the same thing over and over again, you can create a method by defining it with `def`.
 
 1. Create a method named `hello` that will print `"Sam I am"`.
+```
+def hello
+  "Sam I am"
+end  
+```
 
 1. Create a method named `hello_someone` that takes an argument of `name` and prints `"#{name} I am"`.
 
 1. How would you call or execute the method that you created above?
 
+```
+def name(hello_someone)
+  puts "#{name} I am"
+end
+```
+
 1. What questions do you have about methods in Ruby?
+
+I need lots of practice with methods and arguments but it's starting to make more sense!

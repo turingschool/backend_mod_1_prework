5f631e9a9bf4faeeab8bd5b575cc2add339eff2a
deebot10 <dee1jhill@gmail.com>
2021-03-05 10:17:34 -0700

    Final prep

--- final_prep/annotations.rb
+++ final_prep/annotations.rb
@@ -4,11 +4,11 @@
 
 # Build a Bear
 
-def build_a_bear(name, age, fur, clothes, special_power)
-  greeting = "Hey partner! My name is #{name} - will you be my friend?!"
-  demographics = [name, age]
-  power_saying = "Did you know that I can #{special_power}?"
-  built_bear = {
+def build_a_bear(name, age, fur, clothes, special_power)   # We create a method called build_a_bear
+  greeting = "Hey partner! My name is #{name} - will you be my friend?!" # using assignment with interpolation
+  demographics = [name, age] # creating an array demographics
+  power_saying = "Did you know that I can #{special_power}?"  # Assignmeent
+  built_bear = {    # A hash  called bulid_bear
     'basic_info' => demographics,
     'clothes' => clothes,
     'exterior' => fur,
@@ -16,18 +16,18 @@ def build_a_bear(name, age, fur, clothes, special_power)
     'sayings' => [greeting, power_saying, "Goodnight my friend!"],
     'is_cuddly' => true,
   }
-  return built_bear
+  return built_bear # return varible
 end
 
-build_a_bear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares')
-build_a_bear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in')
+build_a_bear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares') # calling the method with arguments
+build_a_bear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in')  # calling the method with arguments
 
 
 # FizzBuzz
 
-def fizzbuzz(num_1, num_2, range)
+def fizzbuzz(num_1, num_2, range)  # Creaated a new method with 3 parameters
   (1..range).each do |i|
-    if i % num_1 === 0 && i % num_2 === 0
+    if i % num_1 === 0 && i % num_2 === 0  # An if statement with conditionals
       puts 'fizzbuzz'
     elsif i % num_1 === 0
       puts 'fizz'
@@ -39,5 +39,5 @@ def fizzbuzz(num_1, num_2, range)
   end
 end
 
-fizzbuzz(3, 5, 100)
+fizzbuzz(3, 5, 100) # Our arguments for fizzbuzz
 fizzbuzz(5, 8, 400)

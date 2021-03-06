995e1ffdbee915b1c365a3b9c2fe8d384bfba2a2
Sarah Rudy <sarahrudy@gmail.com>
2021-03-05 17:34:35 -0700

    Mod Zero Hero

--- final_prep/mod_zero_hero.rb
+++ final_prep/mod_zero_hero.rb
@@ -2,39 +2,81 @@
 
 # Declare two variables - hero_name AND special_ability - set to strings
 
+hero_name = "Mr. Incredible"
+special_ability = "have super strength"
+
 # Declare two variables - greeting AND catchphrase
 #   greeting should be assigned to a string that uses interpolation to include the hero_name
 #   catchphrase should be assigned to a string that uses interpolation to include the special_ability
 
+greeting = "Hi, I'm #{hero_name}!"
+catchphrase = "I #{special_ability}!"
+
 # Declare two variables - power AND energy - set to integers
+
+power = 1000
+energy = 100
 
 # Declare two variables - full_power AND full_energy
 #   full_power should multiply your current power by 500
 #   full_energy should add 150 to your current energy
 
+full_power = (power * 500)
+full_energy = (energy + 150)
+
 # Declare two variables - is_human and identity_concealed - assigned to booleans
 
+is_human = true
+identity_concealed = true
 
 # Declare two variables - arch_enemies AND sidekicks
 #   arch_enemies should be an array of at least 3 different enemy strings
 #   sidekicks should be an array of at least 3 different sidekick strings
 
+arch_enemies = ["Buddy Pine", "Syndrome", "Mirage"]
+sidekicks = ["Elastigirl", "Dash", "Jack Jack Par", "Frozone"]
+
 # Print the first sidekick to your terminal
+
+puts sidekicks[0]
 
 # Print the last arch_enemy to the terminal
 
+puts arch_enemies[2]
+
 # Write some code to add a new arch_enemy to the arch_enemies array
+
+arch_enemies.append("Evelyn Deavor")
 
 # Print the arch_enemies array to terminal to ensure you added a new arch_enemey
 
+p arch_enemies
+
 # Remove the first sidekick from the sidekicks array
+
+sidekicks.delete("Elastigirl")
 
 # Print the sidekicks array to terminal to ensure you added a new sidekick
 
+p sidekicks
+
 # Create a function called assess_situation that takes three arguments - danger_level, save_the_day, bad_excuse
 #   - danger_level should be an integer
-#   - save_the_day should be a string a hero would say once they save the day 
+#   - save_the_day should be a string a hero would say once they save the day
 #   - bad_excuse should be a string a hero would say if they are too afraid of the danger_level
+
+def assess_situation(danger_level, save_the_day, bad_excuse)
+
+  if danger_level > 50
+    puts bad_excuse
+  elsif danger_level > 10 && danger_level < 50
+    puts save_the_day
+  else
+    puts "Meh. Hard pass."
+  end
+end
+
+p assess_situation(51, "Showtime!", "Can't be bothered.")
 
 # Your function should include an if/else statement that meets the following criteria
 #   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the bad_excuse to the terminal
@@ -44,9 +86,9 @@
 #Test Cases
 announcement = 'Never fear, the Courageous Curly Bracket is here!'
 excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
-# assess_situation(99, announcement, excuse) > Should print - 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
-#assess_situation(21, announcement, excuse) > should print - 'Never fear, the Courageous Curly Bracket is here!'
-#assess_situation(3, announcement, excuse) > should print - "Meh. Hard pass."
+assess_situation(99, announcement, excuse) # should print - 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
+assess_situation(21, announcement, excuse) # should print - 'Never fear, the Courageous Curly Bracket is here!'
+assess_situation(3, announcement, excuse) # should print - "Meh. Hard pass."
 
 # Declare a new variable - scary_monster - assigned to an hash with the following key/values
 #   - name (string)
@@ -56,29 +98,76 @@ excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 #   - luckyNumbers (array)
 #   - address (hash with following key/values: number , street , state, zip)
 
+scary_monster = {
+  name: "Sarah",
+  smell: "delicious",
+  weight: 150,
+  cities_destroyed: ["Prague", "Berlin"],
+  lucky_numbers: [2, 9],
+  address: {number: 1403,
+            street: "Albion",
+            state: "CO",
+            zip: 80220}
+}
 
 # Create a new class called SuperHero
 # - Your class should have the following DYNAMIC values
-#   - name 
+#   - name
 #   - super_power
-#   - age 
+#   - age
 # - Your class should have the following STATIC values
 #   - arch_nemesis, assigned to "The Syntax Error"
 #   - power_level = 100
-#   - energy_level = 50 
+#   - energy_level = 50
+
+class Superhero
+  attr_reader :name, :super_power, :age
+  def initialize(name, super_power, age)
+    @name         = name
+    @super_power  = super_power
+    @age          = age
+    @arch_nemesis = "Syndrome"
+    @power_level  = 100
+    @energy_level = 50
+  end
+
 
 # - Create the following class methods
 #   - say_name, should print the hero's name to the terminal
 #   - maximize_energy, should update the energy_level to 1000
 #   - gain_power, should take an argument of a number and INCREASE the power_level by that number
 
+  def say_name
+    @name
+  end
+
+  def maximize_energy
+    @energy_level = 1000
+  end
+
+  def gain_power(power)
+    @power_level = power_level + power
+  end
+end
+
 # - Create 2 instances of your SuperHero class
 
+mr. incredible = SuperHero.new ("Mr. Incredible", "super strength")
+frozone = SuperHero.new ("Frozone", "make ice")
+
+p mr. incredible
+p frozone
 
 # Reflection
 # What parts were most difficult about this exerise?
 
+# I had a really hard time with this: "gain_power, should take an argument of a number and INCREASE the power_level by that number".
+# Also, the extra pressure of feeling like this "should" only take 30 minutes was adding extra frustration, but that's a good lesson on what "the wild" will be like.
+
 # What parts felt most comfortable to you?
+
+# I feel great with hashes and arrays and returning certain elements.
 
 # What skills do you need to continue to practice before starting Mod 1?
 
+# Understanding the purpose of ""@name     = name". I think that makes it a dynamic value, where you can replace name with something else in the code and it will still work, but I'm not sure.

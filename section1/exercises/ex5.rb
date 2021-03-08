01bcf3046398eb2c8d58f7e492afea2078947594
Miriam Aceves <miriammerari02@gmail.com>
2021-03-07 19:42:22 -0500

    Add example ex5.rb

---
+++ section1/exercises/ex5.rb
@@ -0,0 +1,26 @@
+# create variable called types_of_people with value of 10
+types_of_people = 10
+# create a variable x with variable inside the variable
+x = "There are #{types_of_people} types of people."
+# create variable binary with value binary
+binary = "binary"
+# create variable do_not  with value don't
+do_not = "don't"
+# create variable y with two variables inside
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said #{x}."
+puts "I also said '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e

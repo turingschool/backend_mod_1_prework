cb21287a9e1f0038d843b56ade37ea35f994436b
Jermaine Braumuller <braumuller02@gmail.com>
2021-03-20 22:59:38 +0100

    Adds Section 2 work

--- section2/exercises/methods.rb
+++ section2/exercises/methods.rb
@@ -12,16 +12,26 @@ print_name
 
 # Write a method that takes a name as an argument and prints it:
 def print_name(name)
-  # YOUR CODE HERE
+  p "#{name}"
 end
 
 print_name("Albus Dumbledore")
 
-# Write a method that takes in 2 numbers as arguments and prints 
+# Write a method that takes in 2 numbers as arguments and prints
 # their sum. Then call your method:
-# YOUR CODE HERE
+def number_add(a, b)
+  p a + b
+end
 
-# Write a method that takes in two strings as arguments and prints 
-# a concatenation of those two strings. Example: The arguments could be 
-# (man, woman) and the end result might output: "When Harry Met Sally".  
+number_add(5, 6)
+
+# Write a method that takes in two strings as arguments and prints
+# a concatenation of those two strings. Example: The arguments could be
+# (man, woman) and the end result might output: "When Harry Met Sally".
 # Then call your method:
+
+def office_supplies(book, stationairy)
+  puts "Where is the #{book} I lent you? I left my favorite #{stationairy} in it."
+end
+
+office_supplies("textbook", "pen")

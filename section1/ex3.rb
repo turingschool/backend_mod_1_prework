bb57584817ff4a33ec1a258542f9e64e0b0c4213
Marla Schulz <marlaa.schulz@gmail.com>
2021-03-27 16:57:35 -0700

    Add ex1, ex2, ex3

---
+++ section1/ex3.rb
@@ -0,0 +1,47 @@
+# + plus
+# - minues
+# / slash
+# * asterisk
+# % percent
+# < less-than
+# > greater-than
+# <= less-than-equal
+# >= greater-than-equal
+
+#logs string to terminal
+puts "I will not count my chickens:"
+
+#calculates number of hens within a string
+puts "Hens #{25 + 30 /6}"
+#calculates number of roosters within a string
+puts "Roosters #{100 - 25 * 3 % 4}}"
+
+#logs string to terminal
+puts "Now I will count the egss:"
+
+#adds, subtracts, grabs the remainder, and divivdes multiple numbers
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#logs string to terminal
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#adds two numbers then compares if they are less than the subtracted two numbers
+puts 3 + 2 < 5 - 7
+
+# logs string w/ mathematical operation at end.
+puts "What is 3 + 2? #{3 + 2}"
+# logs string w/ mathematical operation at end.
+puts "What is 5 - 7? #{5 - 7}"
+
+#logs string to terminal
+puts "Oh, that's why it's false."
+
+#logs string to terminal
+puts " How about some more."
+
+#logs string with greater than comparison
+puts "Is it greater? #{5 > -2}"
+#logs string with greater than equal to comparison
+puts "Is it greater or equal? #{5 >= -2}"
+#logs string with less than equal to comparison
+puts "Is it less or equal? #{5 <= -2}"

a0dea2a3f96f3d1260d96c4c92c49fd4f6d9ac7d
Marla Schulz <marlaa.schulz@gmail.com>
2021-03-27 17:28:25 -0700

    Create height_cm and weight_kg variables

--- section1/ex5.rb
+++ section1/ex5.rb
@@ -6,6 +6,9 @@ eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
+height_cm = height * 2.54
+weight_kg = weight * 0.45359237
+
 puts "Let's talk about #{name}."
 puts "He's #{height} inches tall."
 puts "He's #{weight} pounds heavy."
@@ -15,3 +18,6 @@ puts "His teeth are usually #{teeth} depending on the coffee"
 
 # this line is tricky, try to get it exactly right
 puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}"
+
+puts height_cm
+puts weight_kg

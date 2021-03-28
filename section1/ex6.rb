0693edb465ae183999c66b5cf40f6d77f97f25a7
Marla Schulz <marlaa.schulz@gmail.com>
2021-03-27 17:44:48 -0700

    Add in study drills

--- section1/ex6.rb
+++ section1/ex6.rb
@@ -1,21 +1,40 @@
+#declare variable
 types_of_people = 10
+#declare variable x with string that includes types_of_people
 x = "There are #{types_of_people} types of people."
+#declare binary variable
 binary = "binary"
+# declare do_not variable
 do_not = "don't"
+# declare variable y with string that include binary and do_not
 y = "Those who know #{binary} and those who #{do_not}."
 
+#prints variable x
 puts x
+#prints variable y
 puts y
 
+#prints string with x variable
 puts "I said: #{x}."
+#prints string with y variable
 puts "I also said: '#{y}'."
 
+#declare variable with boolean
 hilarious = false
+#declare variable with string that incudes hilarious
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
+# print joke_evaluation
 puts joke_evaluation
 
+# declare w variable
 w = "This is the left side of..."
+# declare e variable
 e = "a string with a right side."
 
+# add string w to string e
 puts w + e
+
+# You're connecting the two strings together.
+puts 'This is a string'
+puts "This is another string"

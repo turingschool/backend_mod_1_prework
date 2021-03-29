a4d15cf58c34169fbab7cef594958acf36baedef
Marla Schulz <marlaa.schulz@gmail.com>
2021-03-29 12:52:49 -0700

    Revert to original exercise

--- section2/exWhatIf.rb
+++ section2/exWhatIf.rb
@@ -1,12 +1,12 @@
-people = 15
+people = 20
 cats = 30
 dogs = 15
 
-if people == cats
+if people < cats
   puts "Too many cats! The world is doomed!"
 end
 
-if people != cats
+if people > cats
   puts "Not many cats! The world is saved!"
 end
 

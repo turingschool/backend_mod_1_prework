2f626226aa1803cd5e779c7ad1de721f7022e397
Marla Schulz <marlaa.schulz@gmail.com>
2021-03-31 12:59:10 -0600

    Add study drills

--- section2/exercises/methods_return_values.rb
+++ section2/exercises/methods_return_values.rb
@@ -16,7 +16,7 @@ end
 def divide(a, b)
   puts "DIVIDING #{a} / #{b}"
   return a / b
-end 
+end
 
 
 puts "Let's do some with with just functions!"
@@ -35,3 +35,36 @@ puts "Here is a puzzle."
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
 puts "That becomes: #{what}. Can you do it by hand?"
+
+# my method with returns
+def return_play(arg1, arg2)
+  return arg1 + arg2
+end
+
+# define some variables with return
+string = return_play('hello', 'world')
+number = return_play(4, 2)
+puts "input things now >"
+input = return_play(gets.chomp, gets.chomp)
+
+puts string
+puts number
+puts input
+
+# puzzle
+#add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+puzzle_formula = age + (height - (weight * (iq / 2)))
+puts puzzle_formula
+
+new_what = add(age, multiply(height, subtract(weight, divide(iq, 2))))
+puts new_what
+
+
+# simple formula to method
+
+simple_formula = height + weight * (iq - age)
+with_methods = add(height, multiply(weight, subtract(iq, age)))
+
+puts simple_formula
+puts with_methods

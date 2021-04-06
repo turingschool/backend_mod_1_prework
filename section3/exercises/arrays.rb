f35cd079e61d48bbff06910c22ba84cf05862a51
Marla Schulz <marlaa.schulz@gmail.com>
2021-04-05 17:09:45 -0700

    Complete arrays exercise

--- section3/exercises/arrays.rb
+++ section3/exercises/arrays.rb
@@ -23,42 +23,52 @@ print animals[0]
 
 # YOU DO: Write code below that will print the number of elements in array of
 # animals from above.
-
+puts animals.length
 
 # YOU DO: Write code that will reassign the last item in the animals
 # array to "Gorilla"
-
+animals[2] = "Gorilla"
 
 # YOU DO: Write code that will add a new animal (type of your choice) to position 3.
+animals << "Monkey"
 
 
 # YOU DO: Write code that will print the String "Elephant" in the animals array
-
+animals[2] = "Elephant"
+puts animals[2]
 
 #-------------------
 # PART 2: Foods: Array Methods
 #-------------------
 
 # YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+breakfast_foods = ["waffles", "bananas", "eggs", "pancakes"]
 
 # YOU DO: Write code below that will print the number of elements in the array of
 # foods from above.
-
+breakfast_foods.length
 
 # YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 # print the changed array to verify "broccoli" has been added
+def add_broccoli(array)
+  array << "broccoli"
+  puts array
+end
 
+add_broccoli(breakfast_foods)
 
 # YOU DO: Write code below that removes the last item of food from the foods array and
 # print the changed array to verify that item has been removed
+breakfast_foods.pop
+puts breakfast_foods
 
-
-# YOU DO: Write code to add 3 new foods to the array. 
+# YOU DO: Write code to add 3 new foods to the array.
   # There are several ways to do this - choose whichever you'd like!
 # Then, print the changed array to verify the new items have been added
-
+breakfast_foods.push("syrup", "bacon", "toast")
+puts breakfast_foods
 # YOU DO: Remove the food that is in index position 0.
+breakfast_foods.shift
 
 #-------------------
 # PART 3: Where are Arrays used?
@@ -77,11 +87,9 @@ print animals[0]
 posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 # YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-# may be storing data? Come up with 3 examples - they could be from different web applications or 
+# may be storing data? Come up with 3 examples - they could be from different web applications or
 # all from the same one.
 
-# 1:
-# 2:
-# 3:
-
-
+# 1: Playlist in Spotify
+# 2: Who your friends are on facebook
+# 3: Contacts in an email service

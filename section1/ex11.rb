78d8613e51f4710acb3b56fc12a71a34aa163395
Tanner Dale <tanner@tannerdale.com>
2021-04-07 18:37:01 -0500

    Refractor variable names in exercises 1, 5, and 11

--- section1/ex11.rb
+++ section1/ex11.rb
@@ -2,7 +2,7 @@ print "How old are you? "
 age = gets.chomp
 print "How tall are you? "
 height = gets.chomp
-# height = gets.chomp.to_i 
+# height = gets.chomp.to_i
 print "How much do you weigh? "
 weight = gets.chomp
 
@@ -11,9 +11,9 @@ puts "So you're #{age} old, #{height} tall and #{weight} heavy."
 
 # Study Drills #
 
-print "What is your favourite food? "
-fav_food = gets.chomp
+print "What is your favorite food? "
+fave_food = gets.chomp
 print "What is your favorite time to eat #{fav_food}? "
-fav_time = gets.chomp
+fave_time = gets.chomp
 
-puts "So, you love eating #{fav_food} at #{fav_time}. That sounds like a good time."
+puts "So, you love eating #{fave_food} at #{fave_time}. That sounds like a good time."
--- section1/ex5.rb
+++ section1/ex5.rb
@@ -1,19 +1,19 @@
 name = "Zed A. Shaw"
 age = 35
-height = 74  # inches
-height_cm = height * 2.54  # centimeters
-weight = 180  # lbs
-weight_kilo = weight * 0.453592  # kilograms
+height_in = 74  # inches
+height_cm = height_in * 2.54  # centimeters
+weight_lbs = 180  # lbs
+weight_kilo = weight_lbs * 0.453592  # kilograms
 eyes = "Blue"
 teeth = "White"
 hair = "Brown"
 
 puts "Lets talk about #{name}."
-puts "He's #{height} inches tall."
-puts "He's #{weight} pounds heavy."
+puts "He's #{height_in} inches tall."
+puts "He's #{weight_lbs} pounds heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."
 
 # This line is tricky
-puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+puts "If I add #{age}, #{height_in}, and #{weight_lbs} I get #{age + height_in + weight_lbs}."

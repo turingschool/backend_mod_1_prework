99cdcd78852021a68818119880ae24b28b9692d5
Caroline Tan <81220681+carolinectan@users.noreply.github.com>
2021-04-07 19:55:05 -0400

    Update strings.rb

--- section1/exercises/strings.rb
+++ section1/exercises/strings.rb
@@ -7,9 +7,11 @@
 p "Alan Turing"
 
 # Write code that prints `Welcome to Turing!` to the terminal:
-p #YOUR CODE HERE
+p "Welcome to Turing!"
 
 # Write code that prints `99 bottles of pop on the wall...` to the terminal:
-# YOUR CODE HERE
+p "99 bottles of pop on the wall.."
 
 # Write out code to log one line from your favorite song or movie.
+favorite_line = "That is so fetch!"
+p favorite_line

500adc276b8df242356cc17f1a5f057bbd31f87d
Miriam Aceves <miriammerari02@gmail.com>
2021-04-07 18:36:31 -0400

    Update mod_zero_hero.rb

--- final_prep/mod_zero_hero.rb
+++ final_prep/mod_zero_hero.rb
@@ -2,59 +2,92 @@
 
 # Declare two variables - hero_name AND special_ability - set to strings
 
-`rb
 hero_name = "Cassian"
 special_ability = "Super Strength"
-`
+
 
 # Declare two variables - greeting AND catchphrase
 #   greeting should be assigned to a string that uses interpolation to include the hero_name
 #   catchphrase should be assigned to a string that uses interpolation to include the special_ability
 
-```rb
 greeting = "I am #{hero_name} the guardian of the House of Wind"
 catchphrase = "No need to fret! My #{special_ability} never fails me!"
-```
+
 
 # Declare two variables - power AND energy - set to integers
 
-`rb
+
 power = 85
 energy = 95
-`
+
 
 # Declare two variables - full_power AND full_energy
 #   full_power should multiply your current power by 500
 #   full_energy should add 150 to your current energy
 
+full_power = power * 500
+full energy = energy + 150 
+
 # Declare two variables - is_human and identity_concealed - assigned to booleans
+
+is_human = false
+identity_concealed = false
 
 
 # Declare two variables - arch_enemies AND sidekicks
 #   arch_enemies should be an array of at least 3 different enemy strings
 #   sidekicks should be an array of at least 3 different sidekick strings
 
+arch_enemies = ["Amarantha", "King of Hybern", "Eris"]
+sidekicks = ["Rhys", "Azriel", "Mor", "Feyre"]
+
 # Print the first sidekick to your terminal
+
+p sidekicks[0]
 
 # Print the last arch_enemy to the terminal
 
+p arch_enemies[2]
+
 # Write some code to add a new arch_enemy to the arch_enemies array
+
+arch_enemies.push ("Keir")
 
 # Print the arch_enemies array to terminal to ensure you added a new arch_enemey
 
+p arch_enemies
+
 # Remove the first sidekick from the sidekicks array
 
+sidekicks.shift()
+
 # Print the sidekicks array to terminal to ensure you added a new sidekick
+
+p sidekicks 
 
 # Create a function called assess_situation that takes three arguments - danger_level, save_the_day, bad_excuse
 #   - danger_level should be an integer
 #   - save_the_day should be a string a hero would say once they save the day 
 #   - bad_excuse should be a string a hero would say if they are too afraid of the danger_level
 
+def assess_situation()
+  danger_level = 80
+  save_the_day = "No need to worry, Cas is here!"
+  bad_excuse = "I forgot to hide my blades"
+end 
+
 # Your function should include an if/else statement that meets the following criteria
 #   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the bad_excuse to the terminal
 #   - Anything danger_level that is between 10 and 50 should result in printing the save_the_day string to the terminal
 #   - If the danger_level is below 10, it means it is not worth your time and should result in printing the string "Meh. Hard pass." to the terminal.
+
+if danger_level > 50
+  puts "#{bad_excuse}"
+elsif danger_level > 10 < 50
+  puts " #{save_the_day}"
+else
+  puts "Meh. Hard pass."
+end 
 
 #Test Cases
 announcement = 'Never fear, the Courageous Curly Bracket is here!'
@@ -63,13 +96,15 @@ excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 #assess_situation(21, announcement, excuse) > should print - 'Never fear, the Courageous Curly Bracket is here!'
 #assess_situation(3, announcement, excuse) > should print - "Meh. Hard pass."
 
-# Declare a new variable - scary_monster - assigned to an hash with the following key/values
+# Declare a new variable - scary_monster - assigned to a hash with the following key/values
 #   - name (string)
 #   - smell (string)
 #   - weight (integer)
 #   - citiesDestroyed (array)
 #   - luckyNumbers (array)
 #   - address (hash with following key/values: number , street , state, zip)
+
+ scary_monster = 
 
 
 # Create a new class called SuperHero
@@ -81,6 +116,26 @@ excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 #   - arch_nemesis, assigned to "The Syntax Error"
 #   - power_level = 100
 #   - energy_level = 50 
+  
+  class SuperHero
+    attr_accessor :name, :super_power, :age
+    
+    def initialize(name, super_power, age)
+      @name = name
+      @super_power = super_power
+      @age = age
+    end 
+    
+    def say_name
+      puts "Hello, my name is #{name}."
+    end 
+    
+  end 
+
+
+p say_name
+
+   
 
 # - Create the following class methods
 #   - say_name, should print the hero's name to the terminal

001e5b9883940515d901e507cc0591cf249247f3
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-04-08 03:49:54 -0600

    Add exercises

---
+++ section1/ex11.rb
@@ -0,0 +1,10 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+print "What is one of your hobbies?"
+hobby = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy. You also like to #{hobby}."
---
+++ section1/ex5.rb
@@ -0,0 +1,15 @@
+my_name = 'Zed A. Shaw'
+my_age = 35
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Lets talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} punds heavy."
+puts "Actyually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."

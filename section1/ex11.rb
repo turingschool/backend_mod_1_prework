01791b2054b8e5a7bd6cad3e0a49c6a827c8d700
Jessica Jones <daddehjones@gmail.com>
2021-04-08 14:25:29 -0400

    add ex11

---
+++ section1/ex11.rb
@@ -0,0 +1,18 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+# gets.chomp collects the info from the user.
+print "Where do you live? "
+home = gets.chomp
+print "What do you drive? "
+car = gets.chomp
+print "What do you like to drink? "
+drink = gets.chomp
+
+puts "So, you live in #{home}, drive a #{car} and like to drink #{drink}."

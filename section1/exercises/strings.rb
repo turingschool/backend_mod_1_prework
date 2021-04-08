36ead8bfe71e5056e7fc2cb7cfdc4e917e922564
Jessica Jones <daddehjones@gmail.com>
2021-04-08 15:49:36 -0400

    initial commit

--- section1/exercises/strings.rb
+++ section1/exercises/strings.rb
@@ -4,12 +4,16 @@
 # `ruby section1/exercises/strings.rb`
 
 # Example: Write code that prints your name to the terminal:
-p "Alan Turing"
+name = "JJ"
+puts name
 
 # Write code that prints `Welcome to Turing!` to the terminal:
-p #YOUR CODE HERE
+puts "Welcome to Turing!"
 
 # Write code that prints `99 bottles of pop on the wall...` to the terminal:
-# YOUR CODE HERE
+puts "99 bottles of pop on the wall..."
+
 
 # Write out code to log one line from your favorite song or movie.
+movie_quote = "She doesn't even go here!"
+puts "My favorite line from Mean Girls is #{movie_quote}"

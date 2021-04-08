2f333d9091c5d8a585351de5aa75837adad9ce69
Marla Schulz <marlaa.schulz@gmail.com>
2021-04-07 17:12:35 -0700

    Complete mod zero hero in under 30 min

--- final_prep/mod_zero_hero.rb
+++ final_prep/mod_zero_hero.rb
@@ -1,52 +1,81 @@
 # Challenge - See if you can follow the instructions and complete the exercise in under 30 minutes!
 
 # Declare two variables - hero_name AND special_ability - set to strings
+hero_name = "Trainwreck"
+special_ability = "Super Speed"
 
 # Declare two variables - greeting AND catchphrase
 #   greeting should be assigned to a string that uses interpolation to include the hero_name
 #   catchphrase should be assigned to a string that uses interpolation to include the special_ability
+greeting = "Hey! My name is #{hero_name}!"
+catchphrase = "Catch me with my #{special_ability}."
 
 # Declare two variables - power AND energy - set to integers
+power = 8
+energy = 20
 
 # Declare two variables - full_power AND full_energy
 #   full_power should multiply your current power by 500
 #   full_energy should add 150 to your current energy
+full_power = power * 500
+full_energy = energy + 150
 
 # Declare two variables - is_human and identity_concealed - assigned to booleans
-
+is_human = true
+identity_concealed = false
 
 # Declare two variables - arch_enemies AND sidekicks
 #   arch_enemies should be an array of at least 3 different enemy strings
 #   sidekicks should be an array of at least 3 different sidekick strings
+arch_enemies = ['Slime Boy', 'Monster Man', 'Slither Styles']
+sidekicks = ['Cool Peep', 'Hammer Mamma', 'Power Plug']
 
 # Print the first sidekick to your terminal
+puts sidekicks[0]
 
 # Print the last arch_enemy to the terminal
+puts arch_enemies[2]
 
 # Write some code to add a new arch_enemy to the arch_enemies array
+arch_enemies << 'Danger Dave'
 
 # Print the arch_enemies array to terminal to ensure you added a new arch_enemey
+puts arch_enemies
 
 # Remove the first sidekick from the sidekicks array
+sidekicks.shift
 
 # Print the sidekicks array to terminal to ensure you added a new sidekick
+puts sidekicks
 
 # Create a function called assess_situation that takes three arguments - danger_level, save_the_day, bad_excuse
 #   - danger_level should be an integer
-#   - save_the_day should be a string a hero would say once they save the day 
+#   - save_the_day should be a string a hero would say once they save the day
 #   - bad_excuse should be a string a hero would say if they are too afraid of the danger_level
+
 
 # Your function should include an if/else statement that meets the following criteria
 #   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the bad_excuse to the terminal
 #   - Anything danger_level that is between 10 and 50 should result in printing the save_the_day string to the terminal
 #   - If the danger_level is below 10, it means it is not worth your time and should result in printing the string "Meh. Hard pass." to the terminal.
-
+def assess_situation(danger_level, save_the_day, bad_excuse)
+  if danger_level > 50
+    puts bad_excuse
+  elsif danger_level <= 50 && danger_level >= 10
+    puts save_the_day
+  else
+    puts "Meh. Hard pass."
+  end
+end
 #Test Cases
 announcement = 'Never fear, the Courageous Curly Bracket is here!'
 excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 # assess_situation(99, announcement, excuse) > Should print - 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 #assess_situation(21, announcement, excuse) > should print - 'Never fear, the Courageous Curly Bracket is here!'
 #assess_situation(3, announcement, excuse) > should print - "Meh. Hard pass."
+assess_situation(99, "Never fear, the Courageous Curly Bracket is here!", "I think I forgot to lock up my 1992 Toyota Corolla. Be right back." )
+assess_situation(21, "Never fear, the Courageous Curly Bracket is here!", "I think I forgot to lock up my 1992 Toyota Corolla. Be right back.")
+assess_situation(3, "Never fear, the Courageous Curly Bracket is here!", "I think I forgot to lock up my 1992 Toyota Corolla. Be right back.")
 
 # Declare a new variable - scary_monster - assigned to an hash with the following key/values
 #   - name (string)
@@ -55,17 +84,51 @@ excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 #   - citiesDestroyed (array)
 #   - luckyNumbers (array)
 #   - address (hash with following key/values: number , street , state, zip)
+scary_monster = {
+  name: "Boogie Woogie",
+  smell: "Beans",
+  weight: 110,
+  cities_destroyed: ['New York', 'San Francisco', 'Abilene'],
+  lucky_numbers: [5, 10, 6],
+  address: {
+    number: 2774,
+    street: 'Pie Street',
+    state: 'California',
+    zip: 90008
+  }
+}
 
 
 # Create a new class called SuperHero
 # - Your class should have the following DYNAMIC values
-#   - name 
+#   - name
 #   - super_power
-#   - age 
+#   - age
 # - Your class should have the following STATIC values
 #   - arch_nemesis, assigned to "The Syntax Error"
 #   - power_level = 100
-#   - energy_level = 50 
+#   - energy_level = 50
+class SuperHero
+  attr_accessor :name, :super_power, :age
+  def initialize(name, super_power, age)
+    @name = name
+    @super_power = super_power
+    @age = age
+    @arch_nemesis = 'The Syntax Error'
+    @power_level = 100
+    @energy_level = 50
+  end
+  def say_name
+    puts name
+  end
+  def maximize_energy
+    @energy_level = 1000
+  end
+  def gain_power(number)
+    @power_level += number
+  end
+
+end
 
 # - Create the following class methods
 #   - say_name, should print the hero's name to the terminal
@@ -74,11 +137,21 @@ excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 
 # - Create 2 instances of your SuperHero class
 
+brain_wash = SuperHero.new('Brain Wash', 'Mind Reading', 32)
+magic_mouse = SuperHero.new('Magic Mouse', 'Invisibility', 41)
+
+brain_wash.say_name
+magic_mouse.say_name
+
+puts brain_wash.maximize_energy
+puts magic_mouse.gain_power(40)
 
 # Reflection
 # What parts were most difficult about this exerise?
+# Remembering the syntax for classes was definitely the toughest part. I was also a little confused about the static values thing, but I think I figured it out in the end.
 
 # What parts felt most comfortable to you?
+#declaring variables as strings, booleans, integers, arrays, all of that feels super comfortable and easy for me.
 
 # What skills do you need to continue to practice before starting Mod 1?
-
+# Classes for sure, and probably reviewing a bit on methods as well. Just to be comfortable with what I can and can't do inside of them. 

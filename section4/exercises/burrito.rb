ffa4294b54c24ecc04972071021935fb1f44f546
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-04-09 22:05:16 -0600

    Add Section 4 work

--- section4/exercises/burrito.rb
+++ section4/exercises/burrito.rb
@@ -1,19 +1,41 @@
-# Add the following methods to this burrito class and 
+# Add the following methods to this burrito class and
 # call the methods below the class:
 # 1. add_topping
 # 2. remove_topping
 # 3. change_protein
 
 class Burrito
-  attr_reader :protein, :base, :toppings
+  attr_accessor :protein
+  attr_reader :base, :toppings
+
   def initialize(protein, base, toppings)
     @protein  = protein
     @base     = base
     @toppings = toppings
   end
+
+  def add_topping(topping)
+    toppings.push(topping)
+    puts "You have added the topping #{topping}."
+  end
+
+  def remove_topping(topping)
+    toppings.delete(topping)
+    puts "You have removed the topping #{topping}."
+  end
+
+  def change_protein(protein)
+    self.protein = protein
+    puts "#{protein} is your new protein."
+  end
+
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
 p dinner.toppings
+
+dinner.add_topping('corn')
+dinner.remove_topping('cheese')
+dinner.change_protein('Chicken')
--- section4/exercises/person.rb
+++ section4/exercises/person.rb
@@ -1,5 +1,31 @@
-# Create a person class with at least 2 attributes and 2 behaviors. 
+# Create a person class with at least 2 attributes and 2 behaviors.
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
-# YOUR CODE HERE
+class Person
+  attr_reader :full_name, :hair_color
+
+  def initialize(full_name, hair_color)
+    @full_name = full_name
+    @hair_color = hair_color
+  end
+
+  def introduction(target)
+    puts "Hello #{target}, I'm #{full_name}. How are you?"
+  end
+
+  def dye_hair(hair_color)
+    @hair_color = hair_color
+    puts "Nice dye job! Your hair is now #{hair_color}!"
+  end
+
+end
+
+reese = Person.new('Reese Witherspoon', 'blonde')
+
+p reese.full_name
+p reese.hair_color
+
+reese.introduction('Kim')
+reese.dye_hair('purple')
+p reese.hair_color

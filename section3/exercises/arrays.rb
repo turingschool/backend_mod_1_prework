cb862e56a7d34d82996a680d80f610d2f29c1fac
Caroline Tan <carolinectan3@gmail.com>
2021-04-09 21:56:37 -0400

    Add rest of Section 3 files

--- section3/exercises/arrays.rb
+++ section3/exercises/arrays.rb
@@ -23,42 +23,52 @@ print animals[0]
 
 # YOU DO: Write code below that will print the number of elements in array of
 # animals from above.
-
+p animals.count
 
 # YOU DO: Write code that will reassign the last item in the animals
 # array to "Gorilla"
-
+animals[2] = "Gorilla"
+p animals
 
 # YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals << "Panda"
+p animals
 
 # YOU DO: Write code that will print the String "Elephant" in the animals array
-
+animals[2] = "Elephant"
+p animals[2]
 
 #-------------------
 # PART 2: Foods: Array Methods
 #-------------------
 
 # YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
-
+foods = ["burger", "taco", "pizza", "pasta"]
+p foods
 # YOU DO: Write code below that will print the number of elements in the array of
 # foods from above.
-
+p foods.count
 
 # YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 # print the changed array to verify "broccoli" has been added
-
+foods << "broccoli"
+p foods
 
 # YOU DO: Write code below that removes the last item of food from the foods array and
 # print the changed array to verify that item has been removed
+ #.delete
+foods.pop
+p foods
 
-
-# YOU DO: Write code to add 3 new foods to the array. 
+# YOU DO: Write code to add 3 new foods to the array.
   # There are several ways to do this - choose whichever you'd like!
 # Then, print the changed array to verify the new items have been added
+foods << "fries" << "sushi" << "burritos"
+p foods
 
 # YOU DO: Remove the food that is in index position 0.
+foods.delete_at(0)
+p foods
 
 #-------------------
 # PART 3: Where are Arrays used?
@@ -77,11 +87,12 @@ print animals[0]
 posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 # YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-# may be storing data? Come up with 3 examples - they could be from different web applications or 
+# may be storing data? Come up with 3 examples - they could be from different web applications or
 # all from the same one.
 
-# 1:
+# 1: clothing store
+product_images = ["image of jeans", "image of sweater", "image of tank top"]
 # 2:
+prices = ["$68.12", "$42.97", "$17.00"]
 # 3:
-
-
+sizes = ["x-small", "small", "medium", "large", "x-large"]

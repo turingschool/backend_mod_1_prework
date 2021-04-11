d7c0fbace8e4ea8d7806eca492639155f515f73e
Elliot Olbright <elliotolbright@gmail.com>
2021-04-11 17:27:48 -0600

    INITIAL COMMIT
    adding the first exercise from Ruby in 100 minutes

---
+++ section2/exercises/whatIf.rb
@@ -0,0 +1,50 @@
+#What if Exercise
+
+people = 30
+cats = 30
+dogs = 15
+
+
+if people < cats
+puts "Too many cats! The world is doomed"
+end
+
+if people > cats
+  puts "not many cats! The world is saved"
+end
+
+if people < dogs
+  puts " The world is drooled on!"
+end
+
+if people > dogs
+  puts "The world is dry"
+end
+
+
+dogs += 5
+
+if people >= dogs
+  puts "people are greater than or equal to dogs."
+end
+
+if people <= dogs
+  puts "People are less than or equal to dogs"
+end
+
+if people == dogs
+  puts "people are dogs"
+end
+
+
+#1 if tells the code to look at the variable determined by premade statments
+#2 The 2 spaces tells the code to know what to say from the `puts` given that the if statement is accurate.
+#3 it doesnt look like anything happens
+#4
+height = 10
+weight = 20
+
+if weight > height
+  puts "you are fat!"
+
+#5 changing the value effects the out put of the if then statemnt. If it is no longer correct then the command will not be outputted

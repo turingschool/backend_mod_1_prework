6a78a2b4c9d25d8d40ba7ddf9c974787b5b85962
Michael Abbott <michael@mjabbottdesign.com>
2021-04-11 08:43:37 -0700

    Refactor Section 1

--- section1/ex1.rb
+++ section1/ex1.rb
@@ -1,9 +1,22 @@
-#puts "Hello World!"
-puts "Hello Again"
-exit
-puts "I like typing this."
-puts "This is fun."
-puts "Yay! Printing."
-puts "I much rather you 'not'."
-puts 'I "said" not to touch this.'
+# puts "Hello World!"
+# puts "Hello Again"
+# puts "I like typing this."
+# puts "This is fun."
+# puts "Yay! Printing."
+# puts "I much rather you 'not'."
+# puts 'I "said" not to touch this.'
+# puts "Another line please."
+
+# puts "Hello World!"
+# puts "Hello Again"
+# puts "I like typing this."
+# puts "This is fun."
+# puts "Yay! Printing."
+# puts "I much rather you 'not'."
+# puts 'I "said" not to touch this.'
 puts "Another line please."
+
+# Study Drills
+# 1. line 8
+# 2. line 17
+# 3. line 1 - 16. # turns everything after it on the line into a comment that ruby will not run

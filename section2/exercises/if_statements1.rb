7dc1072254b28072f3b99430c7c3e9e62b77a31e
Jessica Jones <daddehjones@gmail.com>
2021-04-13 12:53:53 -0400

    initial commit

---
+++ section2/exercises/if_statements1.rb
@@ -0,0 +1,13 @@
+weather = 'sunny'
+
+if weather == 'sunny'
+  p "sunscreen"
+elsif weather == 'rainy'
+  p "umbrella"
+elsif weather == 'snowy'
+  p "coat"
+elsif weather == 'icy'
+  p "yak traks"
+else
+  p "good to go!"
+end

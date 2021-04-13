4d61c5855777f32a36df63ea57eedacb00bac08d
Jessica Jones <daddehjones@gmail.com>
2021-04-13 13:22:57 -0400

    update cups

---
+++ section2/exercises/if_statements3.rb
@@ -0,0 +1,12 @@
+cups_of_flour = 5
+has_sauce = false
+
+if cups_of_flour == 1 and has_sauce == true
+  puts "I cannot make pizza"
+elsif cups_of_flour == 5 and has_sauce == false
+  puts "I cannot make pizza"
+elsif cups_of_flour == 2 and has_sauce == true
+  puts "I can make pizza"
+elsif cups_of_flour == 3 and has_sauce == true
+  puts "I can make pizza"
+end 

0bca817e3ef13c4984d0ebe78803f86664dafb86
Lee Hopper <hopper.lee.j@gmail.com>
2021-04-13 17:25:06 -0600

    Complete lesson 3

---
+++ section1/ex3_3.rb
@@ -0,0 +1,8 @@
+# Find something you need to calculate and write a new .rb file that does it
+
+puts "How many days until we start Mod 0?"
+
+puts "Days remaining in April: #{31 - 13}"
+puts "Days until it starts in May: 10"
+
+puts "Answer: #{31 - 13 + 10}"

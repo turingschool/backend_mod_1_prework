9dcc0d04f034aa5ee18e4e5f0e55343bdb1f643c
Jessica Jones <daddehjones@gmail.com>
2021-04-13 12:17:24 -0400

    update if

--- section2/exercises/if_statements2.rb
+++ section2/exercises/if_statements2.rb
@@ -1,7 +1,12 @@
-num_quarters = 0
+puts "How many quarters do you have?"
+puts "It takes 2 quarters for a gumball"
+
+print "> "
+num_quarters = $stdin.gets.chomp
+
 
 if num_quarters == 2
  puts "I have enough money for a gumball"
 elsif num_quarters < 2
  puts "I don't have enough money for a gumball"
-end 
+end

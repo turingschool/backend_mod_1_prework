e86d7aa6b4484e7f3121fc28d0a1118758ff136d
Jessica Jones <daddehjones@gmail.com>
2021-04-14 12:48:24 -0400

    initial commit

--- section2/exercises/if_statements2.rb
+++ section2/exercises/if_statements2.rb
@@ -2,13 +2,13 @@ puts "How many quarters do you have?"
 puts "It takes 2 quarters for a gumball"
 
 print "> "
-num_quarters = $stdin.gets.chomp
+num_quarters = 5
 
 
 if num_quarters == 2
- puts "I have enough money for a gumball"
+  puts "I have enough money for a gumball"
 elsif num_quarters < 2
- puts "I don't have enough money for a gumball"
+  puts "I don't have enough money for a gumball"
 elsif num_quarters >= 2
   puts "You have plenty!"
 end
--- section2/exercises/methods.rb
+++ section2/exercises/methods.rb
@@ -5,28 +5,41 @@
 
 # Example: Write a method that when called will print your name:
 def print_name
-  p "Severus Snape"
+  p "JJ"
 end
 
 print_name
 
+
 # Write a method that takes a name as an argument and prints it:
 def print_name(name)
-  # YOUR CODE HERE
+   puts "name: #{name}"
 end
 
-print_name("Albus Dumbledore")
+print_name("Jessyka Jones")
 
-# Write a method that takes in 2 numbers as arguments and prints 
+# Write a method that takes in 2 numbers as arguments and prints
 # their sum. Then call your method three times with different arguments passed in:
-# YOUR CODE HERE
+def sum(a, b)
+  puts a + b
+end
 
-# Write a method that takes in two strings as arguments and prints 
-# a concatenation of those two strings. Example: The arguments could be 
-# (man, woman) and the end result might output: "When Harry Met Sally".  
-# Then call your method three times with different arguments passed in. 
+sum(4, 12)
+sum(5, 5)
+sum(15, 5)
 
 
+# Write a method that takes in two strings as arguments and prints
+# a concatenation of those two strings. Example: The arguments could be
+# (man, woman) and the end result might output: "When Harry Met Sally".
+# Then call your method three times with different arguments passed in.
+def best_friends(a, b)
+  puts "My best_friends are #{a} + #{b} "
+end
+
+best_friends("Malloy", "Crystal")
+best_friends("My dog", "My phone")
+best_friends("My partner", "My sister")
 #-------------------
 # PART 3: Naming is Hard
 #-------------------
@@ -36,6 +49,6 @@ print_name("Albus Dumbledore")
 
 # Look at the code you wrote for the previous YOU DO🎈 - what did you name the function, and why?
 # What did you name each parameter, and why?
-
 # EXPLAIN:
-
+# named the function best_friends to specifically name names
+# named the parameters a and b so that I know I need to pass in 2 arguments

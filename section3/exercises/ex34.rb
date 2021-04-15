19b20c4aa521a831231dc44e00d30957d26cacca
Jessica Jones <daddehjones@gmail.com>
2021-04-15 10:15:56 -0400

    initial commit

---
+++ section3/exercises/ex34.rb
@@ -0,0 +1,31 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+    puts "1, The animal at 1 is a... "
+    puts animals[1]
+    puts "2, The 3rd animal is a... "
+    puts animals[2]
+    puts "3, The 1st animal is a... "
+    puts animals[0]
+    puts "4, The animal at 3 is a... "
+    puts animals[3]
+    puts "5, The 5th animal is a..."
+    puts animals[4]
+    puts "6, The animal at 2 is a..."
+    puts animals[2]
+    puts "7, The 6th animal is a..."
+    puts animals[5]
+    puts "8, The animal at 4 is a..."
+    puts animals[4]
+
+    #year 2010 is a ordinal number, there is no zero year
+
+  race_winners = ['steveo', 'mary', 'rodknee', 'leigh', 'jj']
+    puts "The winner at 1 is..."
+    puts race_winners[1]
+    puts "The winner at 0 is..."
+    puts race_winners[0]
+    puts "The 3rd place winner is..."
+    puts race_winners[2]
+    puts "The 4th place winner is..."
+    puts race_winners[3]
+    puts "And last but not least is....!"
+    puts race_winners[4]

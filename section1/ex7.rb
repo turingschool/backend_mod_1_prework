d74ae3007c66d03de4744e96a56f0a84bab04ea3
Lee Hopper <hopper.lee.j@gmail.com>
2021-04-15 17:42:51 -0600

    Complete lesson 7

--- section1/ex7.rb
+++ section1/ex7.rb
@@ -1,8 +1,16 @@
-print "How old are you? "
-age = gets.chomp
-print "How tall are you? "
-height = gets.chomp
-print "How much do you weigh? "
-weight = gets.chomp
+print "What is your first name? "
+first_name = gets.chomp
+print "What is your last name? "
+last_name = gets.chomp
+print "What is your birth month? "
+month = gets.chomp
+print "What day of the month were you born? "
+day = gets.chomp
+print "What year were you born? "
+year = gets.chomp
+print "What town were you born in? "
+town = gets.chomp
+print "What state were you born in? "
+state = gets.chomp
 
-puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+puts "#{first_name} #{last_name} was born on #{month} #{day}, #{year} at the hospital in #{town}, #{state}."

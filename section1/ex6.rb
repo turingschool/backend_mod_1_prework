2ad9e12c65dc69c33498047547f1a10010a6b4c8
Lee Hopper <hopper.lee.j@gmail.com>
2021-04-15 17:11:12 -0600

    Add study drill 1

--- section1/ex6.rb
+++ section1/ex6.rb
@@ -1,21 +1,36 @@
+# Set variable for types of people
 types_of_people = 10
+# Set variable with string that includes the type of ppl variable
 x = "There are #{types_of_people} types of people."
+# Set binary variable as string
 binary = "binary"
+# Set do_not variable as string
 do_not = "don't"
+# Set y variable to a string that includes binary and do_not variable
 y = "Those who know #{binary} and those who #{do_not}."
 
+#Print x
 puts x
+#Print y
 puts y
 
+# Print string with x variable
 puts "I said: #{x}."
+# Pring string with y variable
 puts "I also said: '#{y}'."
 
+#Set hilarious variable to 'false'
 hilarious = false
+#Set joke_evaluation variable to string with hilarious variable
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
+# Print joke_evaluation string
 puts joke_evaluation
 
+# Set w variable to a string
 w = "This is the left side of..."
+# Set e variable to a string
 e = "a string with a right side."
 
+# Print w and e variables combined (2 strings)
 puts w + e

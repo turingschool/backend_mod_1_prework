88491eabe96234e87302bb207680032bad8e336f
Carina Sweet <carina.sweet06@gmail.com>
2021-04-19 08:19:33 -0600

    Ruby Basic Lessons added

---
+++ section1/ruby_basics_lessons/ex3.rb
@@ -0,0 +1,30 @@
+puts "I will now count my chickens:"
+
+# Each calculation is going to show the total in Ruby because of the #{}
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+puts "Now I will count the eggs:"
+
+# The #{} are not needed since it is not part of a string. Will calculate the total. This is an integer.
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# Will calculate the total. This is an integer.
+
+puts 3.0+ 2.0 < 5.0 - 7.0
+
+# Each calculation is going to show the total in Ruby because of the #{}
+
+puts "What is 3 + 2? #{3.0 + 2.0}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+# Each calculation is going to show the total in Ruby because of the #{}
+puts "Is it greater? #{5.0 > -2.0}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
---
+++ section1/ruby_basics_lessons/ex7.rb
@@ -0,0 +1,14 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+
+print "Where did you go up? "
+home_town = gets.chomp
+
+puts "You graduated from #{home_town} High School?"

33886b8869a89bad8247d77eff2160816ec083f9
Lee Hopper <hopper.lee.j@gmail.com>
2021-04-19 17:48:02 -0600

    Update with lesson 30 study drills

--- section2/exercises/ex30.rb
+++ section2/exercises/ex30.rb
@@ -1,27 +1,36 @@
 # Exercise 30: Else and If
 
-people = 30
-cars = 40
+# Variable declariation
+people = 60
+cars = 50
 trucks = 15
 
-if cars > people
+# If more cars than people or trucks, print line
+if cars > people || cars > trucks
     puts "We should take the cars."
+# If less cars than people, print line
 elsif cars < people
     puts "We should not take the cars."
+# If meets none of above criteria, print line
 else
     puts "We can't decide."
 end
 
+# If more trucks than cars, print line
 if trucks > cars
   puts "That's too many trucks."
+# If less tucks than cars, print line
 elsif trucks < cars
   puts "Maybe we could take the trucks."
+# If meets none of above criteria, print line
 else
   puts "We still can't decide"
 end
 
+# If more people than trucks, print line
 if people > trucks
   puts "Alright, let's just take the trucks."
+# If not more people than trucks, print line 
 else
   puts "Fine, Let's stay home then."
 end

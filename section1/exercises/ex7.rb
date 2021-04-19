c0865acb5bc7e25fef0f9207c851feff70fa30f2
Lee Hopper <hopper.lee.j@gmail.com>
2021-04-19 17:35:27 -0600

    Move lesson files to exercise directory

---
+++ section1/exercises/ex7.rb
@@ -0,0 +1,16 @@
+print "What is your first name? "
+first_name = gets.chomp
+print "What is your last name? "
+last_name = gets.chomp
+print "What is your birth month? "
+month = gets.chomp
+print "What day of the month were you born? "
+day = gets.chomp
+print "What year were you born? "
+year = gets.chomp
+print "What town were you born in? "
+town = gets.chomp
+print "What state were you born in? "
+state = gets.chomp
+
+puts "#{first_name} #{last_name} was born on #{month} #{day}, #{year} at the hospital in #{town}, #{state}."

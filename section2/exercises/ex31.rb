98ce0f27090242eb94be3d8b8911e40ea929c555
Lee Hopper <hopper.lee.j@gmail.com>
2021-04-20 17:02:01 -0600

    Write new code for your own game

--- section2/exercises/ex31.rb
+++ section2/exercises/ex31.rb
@@ -1,45 +1,73 @@
 # Exercise 31: Making Decisions
+# Study Drill 2: your own game
 
-puts "You enter a dark room with two doors. Do you go through door #1 or door #2?"
+puts "You are about to enter a perilous maze with 3 friends."
+print "> press enter to continue"
+spacer = $stdin.gets.chomp
 
+puts "What is your first friend's name?"
 print "> "
-door = $stdin.gets.chomp
+friend_1 = $stdin.gets.chomp
 
-if door == "1"
-  puts "There's a giant bear here eating a cheese cake. What do you do?"
-  puts "1. Try to pet the bear and eat the cake."
-  puts "2. Run back and charge through door 2."
-  puts "3. Grab sword off the wall and fight the bear."
+puts "What is your second friend's name?"
+print "> "
+friend_2 = $stdin.gets.chomp
 
-  print "> "
-  bear = $stdin.gets.chomp
+puts "What is your third friend's name?"
+print "> "
+friend_3 = $stdin.gets.chomp
 
-  if bear == "1"
-    puts "The bear attacks and things do not end well for you."
-  elsif bear == "2"
-    puts "You step right into the endless abyss and fall Gandalf style."
-  elsif bear == "3"
-    puts "A glorious fight. You take immense damage but kill the bear and escape through the room."
-  else
-    puts "The bear smells your indecision and charges and eats you." % bear
-  end
+puts "You, #{friend_1}, #{friend_2}, and #{friend_3} enter the maze."
+puts "Immediately, a there is an explosion behind you. #{friend_2} screams as lava begins pouring in from where you entered."
+puts "As the lava slowly advaces, you run forward and reach an intersection."
+puts "You are the leader, do you go left or right?"
+print "> "
+decision_1 = $stdin.gets.chomp
 
-elsif door == "2"
-  puts "You stare into the endless abyss at Cthulhu's retina."
-  puts "1. Blueberries."
-  puts "2. Yellow jacket closthespins."
-  puts "3. Understanding revolvers yelling melodies."
+if decision_1 == "left" || decision_1 == "Left"
+  puts "The group advances safetly and you reach another intersection"
+  puts "You are the leader, do you go left or right?"
 
   print "> "
+  decision_2 = $stdin.gets.chomp
 
-  insanity = $stdin.gets.chomp
+  if decision_2 == "right" || decision_2 == "Right"
+    puts "The group advances safetly and you reach another intersection"
+    puts "You are the leader, do you go left or right?"
 
-  if insanity == "1" || insanity == "2"
-    puts "Your body survives powered by a mind of jello. Good job!"
+    print "> "
+    decision_3 = $stdin.gets.chomp
+
+    if decision_3 == "left" || decision_3 == "Left"
+      puts "Daylight! You've escaped. But sadly, it appears #{friend_2} didn't make it, you aren't sure where you lost them..."
+
+    elsif decision_3 == "right" || decision_3 == "Right"
+      puts "The group advances safetly and you reach another intersection"
+      puts "You are the leader, do you go left or right?"
+
+      print "> "
+      decision_4 = $stdin.gets.chomp
+
+      if decision_4 == "right" || decision_4 == "Right"
+        puts "Dead end. No turning back this time, the lava gets all of you."
+      elsif decision_4 == "left" || decision_4 == "Left"
+        puts "Dead end. No turning back this time, the lava gets all of you."
+      else
+        puts "Since you couldn't give the group a straighforward left or right answer, there is mutiny. You are left behind and the lava gets you."
+      end
+
+    else
+      puts "Since you couldn't give the group a straighforward left or right answer, there is mutiny. You are left behind and the lava gets you."
+    end
+
+  elsif decision_2 == "left" || decision_2 == "Left"
+    puts "Dead end. #{friend_1} sprints back and leaps over the lava just in time to get into the other hallway. You, #{friend_1}, and #{friend_2} don't make it."
   else
-    puts "The insanity rots your eyes into a pool of muck. Good job!"
+    puts "Since you couldn't give the group a straighforward left or right answer, there is mutiny. You are left behind and the lava gets you."
   end
 
+elsif decision_1 == "right" || decision_1 == "Right"
+  puts "Dead end. #{friend_3} sprints back and leaps over the lava just in time to get into the other hallway. You, #{friend_1}, and #{friend_2} don't make it."
 else
-  puts "As you fail to make a decision, a bear comes out of door 1 and eats you."
+  puts "Since you couldn't give the group a straighforward left or right answer, there is mutiny. You are left behind and the lava gets you."
 end

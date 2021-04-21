11f4b03f1ef6c7f6067827e84595c797aabe131b
Lee Hopper <hopper.lee.j@gmail.com>
2021-04-21 13:19:08 -0600

    Add example code and study drills

---
+++ section2/exercises/ex21.rb
@@ -0,0 +1,40 @@
+# Exercise 21: Functions Can Return Something
+
+def add(a,b)
+  puts "ADDING #{a} + #{b}"
+  return a + b
+end
+
+def subtract(a,b)
+  puts "SUBTRACTING #{a} - #{b}"
+  return a - b
+end
+
+def multiply(a,b)
+  puts "MULTIPLYING #{a} * #{b}"
+  return a * b
+end
+
+def divide(a,b)
+  puts "DIVIDING #{a} / #{b}"
+  return a / b
+end
+
+puts "Let's do some math with just functions!"
+
+age = add(30,5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
+
+# puzzle example
+puts "Here is a puzzle."
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+puts "That becomes: #{what}. Can you do it by hand?"
+
+# do the inverse, study question 4
+puts "Here is a new puzzle, the inverse of the previous"
+what = divide(iq, multiply(weight, subtract(height, add(age, 2.0))))
+puts "That becomes: #{what}"

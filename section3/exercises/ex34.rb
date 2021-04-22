497a36a79d006edcc558f02305bc221ce0c3a1c1
Sawaez Amin <sawaezamin@gmail.com>
2021-04-21 22:32:03 -0600

    Add Section3 Work

---
+++ section3/exercises/ex34.rb
@@ -0,0 +1,30 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+bear = animals[0]
+ruby = animals[1]
+peacock = animals[2]
+kangaroo = animals[3]
+whale = animals[4]
+platypus = animals[5]
+
+puts "1. (the animal at 1) is the 2nd animal, which is ruby."
+puts "2. (the 3rd animal) is at 2, which is peacock."
+puts "3. (the 1st animal) is at 0, which is bear."
+puts "4. (the animal at 3) is the 4th animal, which is kangaroo."
+puts "5. (the 5th animal) is at 4, which is whale."
+puts "6. (the animal at 2) is the 3rd animal, which is peacock."
+puts "7. (the 6th animal) is at 5, which is platypus."
+puts "8. (the animal at 4) is the 5th animal, which is whale"
+
+puts "check using ruby coding"
+
+
+#Question 1: because of the calendar system. The calendar starts
+# counting at 1 A.D. not at 0. Its ordinal system not a cardinal one.
+puts "1. (the animal at 1) #{animals[1]}."
+puts "2. (the 3rd animal) #{animals[2]}."
+puts "3. (the 1st animal) #{animals[0]}."
+puts "4. (the animal at 3) #{animals[3]}."
+puts "5. (the 5th animal) #{animals[4]}."
+puts "6. (the animal at 2) #{animals[2]}."
+puts "7. (the 6th animal) #{animals[5]}."
+puts "8. (the animal at 4) #{animals[4]}."

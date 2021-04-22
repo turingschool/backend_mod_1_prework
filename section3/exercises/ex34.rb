7d6ab6183c2086a0e8c1aeb9942c76c2c36bc353
Lee Hopper <hopper.lee.j@gmail.com>
2021-04-21 19:39:49 -0600

    Add example code and build study drill 3

---
+++ section3/exercises/ex34.rb
@@ -0,0 +1,22 @@
+# Learn Ruby the Hard Way
+# Exercise 34: Accessing Elemnts of Arrays
+
+# Lesson Example Code
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts animals[1]
+puts animals[2]
+puts animals[0]
+puts animals[3]
+puts animals[4]
+puts animals[2]
+puts animals[5]
+puts animals[4]
+
+# Study Drill 3
+food = ['pizza', 'tacos', 'beer', 'nachos']
+
+puts "The 1st food is at zero and is #{food[0]}"
+puts "The food at 0 is the 1st food and is #{food[0]}"
+puts "The 3rd food is at 2 and is #{food[2]}"
+puts "The food at 2 is the 3rd food and is #{food[2]}"

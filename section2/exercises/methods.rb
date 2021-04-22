f63dc09b2b7c3026071b48211186298b2714c068
Ozzie <mikeosmonson@gmail.com>
2021-04-21 21:03:31 -0600

    add all Mod 1 prework for section 2 including exercises and refection

--- section2/exercises/methods.rb
+++ section2/exercises/methods.rb
@@ -12,19 +12,73 @@ print_name
 
 # Write a method that takes a name as an argument and prints it:
 def print_name(name)
-  # YOUR CODE HERE
+  puts "Albus Dumbledore"
 end
 
 print_name("Albus Dumbledore")
 
-# Write a method that takes in 2 numbers as arguments and prints 
-# their sum. Then call your method three times with different arguments passed in:
-# YOUR CODE HERE
 
-# Write a method that takes in two strings as arguments and prints 
-# a concatenation of those two strings. Example: The arguments could be 
-# (man, woman) and the end result might output: "When Harry Met Sally".  
-# Then call your method three times with different arguments passed in. 
+
+# Write a method that takes in 2 numbers as arguments and prints
+# their sum. Then call your method three times with different arguments passed in:
+def numbers(a, b)
+  puts "First number is #{a}"
+  puts "Second number is #{b}"
+  return a + b
+end
+
+sum = numbers(5, 7)
+puts "The sum of numbers is #{sum}"
+puts ""
+
+variation_1 = numbers(4, 21) - 5
+puts "Variation 1 minus 5 is #{variation_1}"
+puts ""
+
+variation_2 = numbers(11, 10)
+puts "Variation 2 is #{variation_2}"
+puts ""
+
+variation_3 = numbers(27, 93) / 3
+puts "Variation 3 divided by 3 is #{variation_3}"
+puts ""
+
+
+
+
+# Write a method that takes in two strings as arguments and prints
+# a concatenation of those two strings. Example: The arguments could be
+# (man, woman) and the end result might output: "When Harry Met Sally".
+# Then call your method three times with different arguments passed in.
+ def dog_names(name_1, name_2)
+   puts "The dogs are named #{name_1} and #{name_2}."
+   puts "It was hard to give up #{name_1} but we got #{name_2} right after."
+   puts "#{name_1} was a little crazy and #{name_2} is quite mature."
+   puts "We're not suppose to pick favoriets but I'd pick #{name_2} over #{name_1}."
+   puts "" # Added this to create a space in between the different arguments.
+ end
+
+puts "We socialize dogs before they go back to their final leg of service dog training."
+dog_1 = "Burton"
+dog_2 = "Pesto"
+
+dog_names(dog_1, dog_2)
+
+dog_3 = "Spagetti"
+dog_4 = "Burrito"
+
+dog_names(dog_3, dog_4)
+
+dog_5 = "Aragorn"
+dog_6 = "Gandalf"
+
+dog_names(dog_5, dog_6)
+
+dog_7 = "Atreyu"
+dog_8 = "Falkor"
+
+dog_names(dog_7, dog_8)
+
 
 
 #-------------------
@@ -38,4 +92,5 @@ print_name("Albus Dumbledore")
 # What did you name each parameter, and why?
 
 # EXPLAIN:
-
+# In my naming conventions, I was very direct. When I was asked about the sum of numbers, I used numbers as the method and a as the first number and b as the second number. Then I used sum and variations 1, 2, and 3 to name the sum and the variations.
+# As with the names, I wanted to use the names of dogs so I folled a numeric dog labeling. As for the arguments, I used name 1 and 2 because it made sense to me and I figure that anyone reading my test could understand it.
--- section2/reflection.md
+++ section2/reflection.md
@@ -2,28 +2,82 @@
 
 1. Regarding the blog posts in Part A, how do you feel about asking questions? Do you tend to ask them too soon, or wait too long, or somewhere in between?
 
+# I don't usually mind asking questions directly to my instructors but I don't like posting them on slack but it is something that I will have to get over. As long as it contributes to my success, I can make myself do it and not waste time. I have been waiting too long to ask questions but I will change that.
+
 ### If Statements
 
 1. What is a conditional statement? Give three examples.
 
+# A conditional statement that is true or false depending on a set of conditions. Examples below.
+if
+elsif
+else
+
 1. Why might you want to use an if-statement?
+
+When you are creating a solution that does not have a binary answer. If there are multiple choices to be made or there is input required that will determine the path of the program.
 
 1. What is the Ruby syntax for an if statement?
 
+```
+if conditional [then]
+   code...x
+[else
+   code...]
+end
+```
+
+
 1. How do you add multiple conditions to an if statement?
+
+You can add elsif after the initial if statement to continue to add conditions.
 
 1. Provide an example of the Ruby syntax for an if/elsif/else statement:
 
+```fun = 5
+if fun > 5
+  puts "Today is amazing!"
+elsif fun == 5
+  puts "It's fine."
+else
+  puts "I've had better days."
+end
+```
+
 1. Other than an if-statement, can you think of any other ways we might want to use a conditional statement?
+
+I have found that there are unless statements that could be used in place of if statements.
 
 ### Methods
 
 1. In your own words, what is the purpose of a method?
 
+A method is an efficient piece of code that when called will to preform a procedure without having to repeat it over and over. It can be easily assigned again. 
+
 1. Create a method named `hello` that will print `"Sam I am"`.
+
+```
+def hello
+  puts "Sam I am"
+end
+
+hello
+```
 
 1. Create a method named `hello_someone` that takes an argument of `name` and prints `"#{name} I am"`.
 
+```
+def hello_someone(name)
+  puts "#{name} I am"
+end
+
+hello_someone("Ozzie")
+```
+
 1. How would you call or execute the method that you created above?
+
+```
+hello_someone("Ozzie")
+```
 
 1. What questions do you have about methods in Ruby?

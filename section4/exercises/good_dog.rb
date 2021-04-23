b155789ccd11f797844c358968003c0d33e63d1a
Lee Hopper <hopper.lee.j@gmail.com>
2021-04-22 18:16:37 -0600

    Add example code

---
+++ section4/exercises/good_dog.rb
@@ -0,0 +1,81 @@
+# LaunchSchool
+# Classes and Objects - Part 1
+
+# Example Code - with manual getter and setter methods
+
+class GoodDog
+  def initialize(name)
+    @name = name
+  end
+
+  def speak
+    "#{@name} says arf!"
+  end
+
+  def name
+    @name
+  end
+
+  def name=(n)
+    @name = n
+  end
+end
+
+sparky = GoodDog.new("Sparky")
+puts sparky.speak
+puts sparky.name
+sparky.name = "Spartacus"
+puts sparky.name
+
+# Example Code - with attr_accessor method
+
+class GoodDog
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def speak
+    "#{@name} says arf!"
+  end
+end
+
+sparky = GoodDog.new("Sparky")
+puts sparky.speak
+puts sparky.name
+sparky.name = "Spartacus"
+puts sparky.name
+
+
+# Updated example code
+
+class GoodDog
+  attr_accessor :name, :height, :weight
+
+  def initialize(n, h, w)
+    @name = n
+    @height = h
+    @weight = w
+  end
+
+  def speak
+    "#{name} says arf!"
+  end
+
+  def change_info(n, h, w)
+    self.name = n
+    self.height = h
+    self.weight = w
+  end
+
+  def info
+    "#{self.name} weighs #{self.weight} and is #{self.height} tall."
+  end
+end
+
+sparky = GoodDog.new('Sparky', '12 inches', '10 lbs')
+puts sparky.info
+
+sparky.change_info('Spartacus', '24 inches', '45 lbs')
+puts sparky.info

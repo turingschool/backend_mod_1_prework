d6c7f04a2c221224e255a51c43388d3148770a1c
Karan Mehta <karanm645@gmail.com>
2021-04-24 14:57:20 -0500

    Add Section 4 work

--- section4/exercises/burrito.rb
+++ section4/exercises/burrito.rb
@@ -1,4 +1,4 @@
-# Add the following methods to this burrito class and 
+# Add the following methods to this burrito class and
 # call the methods below the class:
 # 1. add_topping
 # 2. remove_topping
@@ -11,9 +11,25 @@ class Burrito
     @base     = base
     @toppings = toppings
   end
+  def add_toppings(item)
+    @toppings << item
+    puts "Your #{item} have been added"
+  end
+
+  def remove_toppings(item)
+     puts "Your #{item} have been removed"
+  end
+
+  def change_protein(protein)
+    puts "Your protein will be changed to #{protein}"
+  end
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
 p dinner.toppings
+
+p dinner.add_toppings('olives')
+p dinner.remove_toppings('mushrooms')
+p dinner.change_protein('chickpeas')
--- section4/exercises/person.rb
+++ section4/exercises/person.rb
@@ -1,5 +1,26 @@
-# Create a person class with at least 2 attributes and 2 behaviors. 
+# Create a person class with at least 2 attributes and 2 behaviors.
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
-# YOUR CODE HERE
+class Person
+  attr_accessor :name, :weight
+
+  def initialize(name)
+    @name = name
+    @weight = weight
+  end
+  def speak
+    "#{name} says hello!"
+  end
+  def jump(number)
+    "Your jump caused #{number} pounds of havoc!"
+  end
+end
+
+human = Person.new('Sean')
+human_2 = Person.new('Jim')
+p human.speak
+p human_2.name
+human.name = "John"
+p human.name
+p human.jump(155)

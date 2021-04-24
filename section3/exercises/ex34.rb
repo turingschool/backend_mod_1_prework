76e7d3b8057f9ffef404e8e1d66a1bea8f91d008
Carina Sweet <carina.sweet06@gmail.com>
2021-04-23 21:21:11 -0600

    Add Section 3 work

---
+++ section3/exercises/ex34.rb
@@ -0,0 +1,31 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts animals[2]
+
+
+#1 The animal at 1 is a ruby
+#2 The third (3rd) animal is at 2 and is a peacock
+#3 The first (1st) animal is at 0 and is a bear
+#4 The animal at 3 is a kangaroo
+#5 The fifth (5th) animal is at 4 and is a whale
+#6 The animal at 2 is a peacock
+#7 The sixth (6th) animal is at 5 and is a platypus
+#8 The animal at 4 is a whale
+
+
+# Study Drills
+
+#1 2010 in January,1 2010 is a ordinal number.
+
+#2
+
+dogs = ['pug', 'shepard', 'saint', 'lab', 'dalmation', "poodle"]
+
+# The dog at 1 is a shepard
+# The third (3rd) dog is at 2 and is a saint
+# The first (1st) dog is at 0 and is a pug
+# The dog at 3 is a lab
+# The fifth (5th) dog is at 4 and is a dalmation
+# The dog at 2 is a saint
+# The sixth (6th) dog is at 5 and is a poodle
+# The dog at 4 is a dalmation

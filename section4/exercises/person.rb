13d34b9fce1edd8eaced058a5c2a393f9242fa9b
Ozzie <57572826+ozzman84@users.noreply.github.com>
2021-04-24 21:41:19 -0600

    Update person.rb

--- section4/exercises/person.rb
+++ section4/exercises/person.rb
@@ -1,5 +1,30 @@
 # Create a person class with at least 2 attributes and 2 behaviors. 
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
+class Person
+  attr_reader :name, :age
 
-# YOUR CODE HERE
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+
+  def speak
+    puts "Hi"
+  end
+
+
+  def introduce
+    puts "My name is #{name} and I'm #{age}."
+  end
+end
+
+
+
+john = Person.new("John", "24")
+p john.name
+p john.age
+john.speak
+john.introduce
+

af19fb8cdb54a1f505509813436cfc90b963d6f1
Kevin Mugele <kevin.mugele@gmail.com>
2021-05-16 19:21:49 -0400

    Add Section 1

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,59 +1,69 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
 # prints what that variable holds to the terminal:
 name = "Harry Potter"
-p name
+puts name
 
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+puts house_elf
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+warning = "Harry Potter must not return to Hogwarts!"
+puts warning
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
-# YOUR CODE HERE
-p students
+students = students + 2
+puts students
+
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
-# YOUR CODE HERE
-p students
+students = students - 2
+puts students
+
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
+first_name = "Kevin"
+is_hungry = "is hungry"
+number_of_pets = 2
+puts "#{first_name} #{is_hungry} and has #{number_of_pets} pets!"
 
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
-
+# How did you decide to use the data type you did for each of the three variables above?
 # Explain.
-
+#For both first_name and is_hungry I chose strings. Both of these variables have characters. For number_of_pets I chose a number for the variable. This doesn't require quotation marks.
 
 # YOU DO:
 # Re-assign the values to the three variables from the previous challenge to different values (but same data type).
 # print all three variables to the terminal.
-
+first_name = "Carol Lee"
+is_hungry = "very hungry"
+number_of_pets = 6
+puts "My mother's name is #{first_name}. She is #{is_hungry}. She has had #{number_of_pets} pets in her life!"
 
 # YOU DO:
 # Using the variables below, print the total number of snacks to the terminal:
 healthy_snacks = 6;
 junk_food_snacks = 8;
+puts "There are #{healthy_snacks + junk_food_snacks} snacks in the fridge!"
 
 
 #-------------------
 # FINAL CHECK
 #-------------------
 
-# Did you run this file in your terminal to make sure everything printed out to the terminal 
-  # as you would expect? 
+# Did you run this file in your terminal to make sure everything printed out to the terminal
+  # as you would expect?

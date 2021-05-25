9b1a7c0adc1236be0457457d77ecc59e76e6f9b8
Laura Court <laura.mcourt@gmail.com>
2021-05-24 21:15:36 -0400

    Change name of file

---
+++ section1/ex3_study_drill.rb
@@ -0,0 +1,7 @@
+puts "Mod 1 prework is about 40 hours."
+puts "Professional developement should take about 2 hours"
+puts "Gear Up should take about 2 hours as well"
+puts "There is also a technical portion. dun. Dun. DUN."
+
+# Find the number of hours needed to complete the technical
+puts "The technical will take about #{40 - 2 - 2} hours."

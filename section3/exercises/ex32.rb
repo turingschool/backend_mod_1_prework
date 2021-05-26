f2353c90b6c89a24773e61e251acbd988b344eac
Laura Court <laura.mcourt@gmail.com>
2021-05-25 21:50:38 -0400

    Add section3 exercises and reflection

---
+++ section3/exercises/ex32.rb
@@ -0,0 +1,43 @@
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# this first kind of for-loop goes through a list
+# in a more traditional style found in other languages
+for number in the_count
+  puts "This is count #{number}"
+end
+
+# same as above, but in a more Ruby style
+# this and the next one are the preferred
+# way Ruby for-loops are written
+fruits.each do |fruit|
+  puts "A fruit of type: #{fruit}"
+end
+
+# also we can go through mixed lists too
+# note this is yet another style, exactly like above
+# but a different syntax (way to write it).
+change.each {|i| puts "I got #{i}" }
+
+# we can also build lists, first start with an empty one
+elements = []
+
+# then use the range operator to do 0 to 5 counts
+(0..5).each do |i|
+  puts "adding #{i} to the list."
+  # pushes the i variable on the *end* of the list
+  elements.push(i)
+end
+
+# now we can print them out too
+elements.each {|i| puts "Element was: #{i}" }
+
+# study drill loop
+the_count.each do |number| # '|number|' declares variable to use in the loop
+  puts "This is count #{number}"
+end
+# '..' creates an inclusive range
+# '...' creates a range that excludes the specified high value
+# '<<' accepts a single argument, and pushes it onto the end of the array.
+# 'push' accepts one or more arguemnts, pushing them all onto the end.
---
+++ section3/exercises/ex34.rb
@@ -0,0 +1,18 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts " #{animals[1]}- The animal at 1 is the second (2nd) animal and is a ruby."
+puts " #{animals[2]}- The third (3rd) animal is at 2 and is a peakcock."
+puts " #{animals[0]}- The first (1st) animal is at 0 and is a bear."
+puts " #{animals[3]}- The animal at 3 is the forth (4th) animal and is a kangaroo."
+puts " #{animals[4]}- The fifth (5th) animal is at 4 and is a whale."
+puts " #{animals[2]}- The animal at 2 is the third (3rd) animal and is a peakcock."
+puts " #{animals[5]}- The sixth (6th) animal is at 5 and is a platypus."
+puts " #{animals[4]}- The animal at 4 is fifth (5th) and is a whale."
+
+
+
+num = [ 2, 4, 6, 78, 256246, 354353, 46364345, 2256, 3452677, 4535]
+p num[1] # returns 4
+p num[8] # returns 3452677
+p num[4] # returns 256246
+p num[6] # returns 46364345

66dc275009a87908bfb1f6300a0de86428acb124
Erika Kischuk <erika.kischuk@gmail.com>
2021-05-26 23:12:03 -0600

    Complete section 3 work

---
+++ section3/exercises/ex34.rb
@@ -0,0 +1,27 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The animal at 1 is the second animal in the array and is ruby.
+puts animals[1]
+# The third animal is at 2 in the array and is a peacock
+puts animals[2]
+# The first animal in the array is at 0 and is a bear
+puts animals[0]
+# The animal at 3 is fourth in the array and is a kangaroo
+puts animals[3]
+# The fifth animal in the array is at 4 and is a whale
+puts animals[4]
+# The animal at 2 is third in the array and is a peacock
+puts animals[2]
+# The animal sixth in the array is at 5 and is a platypus
+puts animals[5]
+# The animal at 4 is fifth in the array and is still a whale
+puts animals[4]
+
+bikini_bottom_residents = ['Spongebob', 'Gary', 'Patrick', 'Squidward', 'Mr. Crabs', 'Sandy']
+
+puts "#{bikini_bottom_residents[1]} is a snail."
+puts "#{bikini_bottom_residents[3]} plays clarinet."
+puts "#{bikini_bottom_residents[5]} can't breath underwater."
+puts "#{bikini_bottom_residents[0]} loves his job."
+puts "#{bikini_bottom_residents[2]} lives under a rock, literally."
+puts "#{bikini_bottom_residents[4]} is a capitalist."

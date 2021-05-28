f645736d0d1c0a26ea3372ced90acc11986f3809
Henry Schmid <henry.schmid1@gmail.com>
2021-05-27 21:07:37 -0600

    Complete Final Prep

--- final_prep/annotations.rb
+++ final_prep/annotations.rb
@@ -3,41 +3,78 @@
 # Use the # to create a new comment
 
 # Build a Bear
-
+# Define a method called build_a_bear with the arguments name, age, fur, clothes,
+# and special_power.
 def build_a_bear(name, age, fur, clothes, special_power)
+# Assign a string to a variable called 'greeting' and interpolate the 'name'
+# variable within the string.
   greeting = "Hey partner! My name is #{name} - will you be my friend?!"
+# Assign an array to a variable called 'demographics' containing the 'say_name'
+# and 'age' variables.
   demographics = [name, age]
+# Assign a string to a variable called 'power_saying' and interpolate the
+# 'special_power' variable.
   power_saying = "Did you know that I can #{special_power}?"
+# Create a hash called built_bear
   built_bear = {
+# Create a key/value pair using a string as the key and the demographics variable as the value.
     'basic_info' => demographics,
+# Create a key/value pair using a string as the key and the clothes variable as the value.
     'clothes' => clothes,
+# Create a key/value pair using a string as the key and the fur variable as the value.
     'exterior' => fur,
+# Create a key/value pair using a string as the key and a float as the value.
     'cost' => 49.99,
+# Create a key/value pair using a string as the key and an array containing two
+# variables and a string as the value.
     'sayings' => [greeting, power_saying, "Goodnight my friend!"],
+# Create a key/value pair using a string as the key and a boolean value as the value.
     'is_cuddly' => true,
+# Curly bracket to close out the hash.
   }
+# Return the built_bear hash.
   return built_bear
+# End the definition of the build_a_bear method.
 end
 
+# Call the build_a_bear method using a string for 'name' argument, integer for 'age',
+# string for 'fur', array for 'clothes', and string for 'special_power'.
 build_a_bear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares')
+# Call the build_a_bear method using the same data types as above but different values.
 build_a_bear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in')
 
 
 # FizzBuzz
-
+# Define a method called fizzbuzz with the arguments num_1, num_2, and range.
 def fizzbuzz(num_1, num_2, range)
+# Iterate through each number in a range from 1 to whatever number is input for
+# the 'range' argument, and do something for each number.
   (1..range).each do |i|
+# Use an if statement that executes code if the conditional is true. If i is
+# divisible by num_1 and num_2, then execute the code below. If not true, move
+# on to the elsif statement.
     if i % num_1 === 0 && i % num_2 === 0
+# Print 'fizzbuzz' if the conditional above is true.
       puts 'fizzbuzz'
+# Use an elsif statement that executes code if the conditional is true. If i is
+# divisible by num_1, then print 'fizz'. If not true, move on to next elsif statement.
     elsif i % num_1 === 0
       puts 'fizz'
+# Use an elsif statement that executes code if the conditional is true. If i is
+# divisible by num_2, then print 'buzz'. If not true, move on to else statement.
     elsif i % num_2 === 0
       puts 'buzz'
+# If none of the above conditional statements are true, print i.
     else
       puts i
+# End the if/elsif/else statement.
     end
+# End the for loop.
   end
+# End the fizzbuzz method definition.
 end
 
+# Call the fizzbuzz method using 3, 5, and 100 as arguments.
 fizzbuzz(3, 5, 100)
+# Call the fizzbuzz method using 5, 8, and 400 as arguments.
 fizzbuzz(5, 8, 400)
--- final_prep/mod_zero_hero.rb
+++ final_prep/mod_zero_hero.rb
@@ -1,45 +1,76 @@
 # Challenge - See if you can follow the instructions and complete the exercise in under 30 minutes!
 
 # Declare two variables - hero_name AND special_ability - set to strings
+hero_name = "Kvothe"
+special_ability = "naming the wind"
 
 # Declare two variables - greeting AND catchphrase
 #   greeting should be assigned to a string that uses interpolation to include the hero_name
 #   catchphrase should be assigned to a string that uses interpolation to include the special_ability
+greeting = "Hello, my name is #{hero_name}."
+catchphrase = "My special ability is #{special_ability}."
 
 # Declare two variables - power AND energy - set to integers
+power = 12
+energy = 30
 
 # Declare two variables - full_power AND full_energy
 #   full_power should multiply your current power by 500
 #   full_energy should add 150 to your current energy
+full_power = power * 500
+full_energy = energy + 150
 
 # Declare two variables - is_human and identity_concealed - assigned to booleans
-
+is_human = true
+identity_concealed = false
 
 # Declare two variables - arch_enemies AND sidekicks
 #   arch_enemies should be an array of at least 3 different enemy strings
 #   sidekicks should be an array of at least 3 different sidekick strings
+arch_enemies = ["Haliax", "Cyphus", "Cinder"]
+sidekicks = ["Simmon", "Wilem", "Fela"]
 
 # Print the first sidekick to your terminal
+puts sidekicks[0]
 
 # Print the last arch_enemy to the terminal
+puts arch_enemies[-1]
 
 # Write some code to add a new arch_enemy to the arch_enemies array
+arch_enemies.push("Stercus")
 
 # Print the arch_enemies array to terminal to ensure you added a new arch_enemey
+p arch_enemies
 
 # Remove the first sidekick from the sidekicks array
+sidekicks.shift
 
 # Print the sidekicks array to terminal to ensure you added a new sidekick
+#    (the prompt above says to remove, not add)
+p sidekicks
 
 # Create a function called assess_situation that takes three arguments - danger_level, save_the_day, bad_excuse
 #   - danger_level should be an integer
-#   - save_the_day should be a string a hero would say once they save the day 
+#   - save_the_day should be a string a hero would say once they save the day
 #   - bad_excuse should be a string a hero would say if they are too afraid of the danger_level
+def assess_situation(danger_level, save_the_day, bad_excuse)
 
 # Your function should include an if/else statement that meets the following criteria
 #   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the bad_excuse to the terminal
 #   - Anything danger_level that is between 10 and 50 should result in printing the save_the_day string to the terminal
 #   - If the danger_level is below 10, it means it is not worth your time and should result in printing the string "Meh. Hard pass." to the terminal.
+if danger_level > 50
+  puts bad_excuse
+elsif danger_level.between?(10, 50)
+  puts save_the_day
+else
+  puts "Meh. Hard pass."
+end
+end
+
+assess_situation(99, "I have defeated the Chandrian!", "I have a tummy ache.")
+assess_situation(21, "I have defeated the Chandrian!", "I have a tummy ache.")
+assess_situation(3, "I have defeated the Chandrian!", "I have a tummy ache.")
 
 #Test Cases
 announcement = 'Never fear, the Courageous Curly Bracket is here!'
@@ -55,30 +86,63 @@ excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 #   - citiesDestroyed (array)
 #   - luckyNumbers (array)
 #   - address (hash with following key/values: number , street , state, zip)
-
+scary_monster = {
+  name: 'Trogdor',
+  smell: 'armpits',
+  weight: 322,
+  citiesDestroyed: ['Paris', 'Denver', 'Antwerp'],
+  luckyNumbers: [1, 2, 3],
+  address: {number: 96, street: 'Burninator St.', state: 'South Dakota', zip: 98765}
+}
 
 # Create a new class called SuperHero
 # - Your class should have the following DYNAMIC values
-#   - name 
+#   - name
 #   - super_power
-#   - age 
+#   - age
 # - Your class should have the following STATIC values
 #   - arch_nemesis, assigned to "The Syntax Error"
 #   - power_level = 100
-#   - energy_level = 50 
+#   - energy_level = 50
+class SuperHero
+  @@arch_nemesis = "The Syntax Error"
+  @@power_level = 100
+  @@energy_level = 50
+
+  attr_accessor :name, :super_power, :age
+  def initialize(name, super_power, age)
+    @name = name
+    @super_power = super_power
+    @age = age
+  end
 
 # - Create the following class methods
 #   - say_name, should print the hero's name to the terminal
 #   - maximize_energy, should update the energy_level to 1000
 #   - gain_power, should take an argument of a number and INCREASE the power_level by that number
+  def say_name
+    puts "The hero's name is #{@name}"
+  end
 
+  def maximize_energy
+    @@energy_level = 1000
+  end
+
+  def gain_power(increase)
+    @@power_level += increase
+  end
+end
 # - Create 2 instances of your SuperHero class
-
+curly_bracket = SuperHero.new("The Courageous Curly Bracket", "fighting errors", 207)
+bug_slayer = SuperHero.new("The Bug Slayer", "squashing bugs", 4)
 
 # Reflection
 # What parts were most difficult about this exerise?
+# The static attributes were the most difficult because I had not encountered them before.
+# A quick Google search helped me with the syntax.
 
 # What parts felt most comfortable to you?
+# Declaring all the variable at the beginning was the most comfortable because I have done this many times.
 
 # What skills do you need to continue to practice before starting Mod 1?
-
+# I will need to continue working with classes to build more muscle memory.

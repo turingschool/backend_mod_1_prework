66f72799555f5e56160a7c3ff402abe4637d1b45
Ida Olson <idaolson@gmail.com>
2021-05-29 16:52:50 -0600

    Add Section 3 work

--- section3/exercises/hashes.rb
+++ section3/exercises/hashes.rb
@@ -1,32 +1,41 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section3/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
 foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
+
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
-p zoo
+zoo_animals = {zebras: 12, hippos: 3, tigers: 5}
+p zoo_animals
 
-# Write code that prints all of the 'keys' of the zoo variable 
+
+# Write code that prints all of the 'keys' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+zoo_animals.each do |animal, amount|
+  puts animal
+end
 
-# Write code that prints all of the 'values' of the zoo variable 
+
+# Write code that prints all of the 'values' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+zoo_animals.each do |animal, amount|
+  puts amount
+end
 
-# Write code that prints the value of the first animal of the zoo variable 
+# Write code that prints the value of the first animal of the zoo variable
 # you created above:
-# YOUR CODE HERE
 
-# Write code that adds an animal to the zoo hash. 
+first_value = zoo_animals.values[0]
+puts first_value
+
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
-
+zoo_animals[:monkeys] = 15
+p zoo_animals
 
 #-------------------
 # Part 2: Email
@@ -39,16 +48,29 @@ p zoo
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
+email = {
+  "to" => "Sarah Carter",
+  "from" => "Ida Olson?,
+  "subject" => "Hello",
+  "date" => "May 28, 2021",
+  "body" => "Hey, Bestie! Miss me?"
+}
+
 # Write code that prints your email hash to the terminal.
+print email
+puts
 
-
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
-# YOUR CODE HERE
+email.each do |key, value|
+  puts key
+end
 
-# Write code that prints all of the 'values' of the email hash 
+# Write code that prints all of the 'values' of the email hash
 # you created above:
-# YOUR CODE HERE
+email.each do |key, value|
+  puts value
+end
 
 
 #-------------------
@@ -64,38 +86,38 @@ p zoo
 # posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 
-# Frankly, that was a very simplified version of the Array the Instagram developers have 
+# Frankly, that was a very simplified version of the Array the Instagram developers have
 # written and work with. Still probably slightly simplified as we don't know what their code
 # actually looks like, but it may look more like this:
+#
+#
+# posts = [
+#   {
+#     'image_src' => "./images/beach.png",
+#     'caption' => "At the beach with my besties",
+#     'timestamp' => "4:37 PM August 13, 2019",
+#     'number_likes' => 0,
+#     'comments' => []
+#   },
+#   {
+#     'image_src' => "./images/holiday-party.png",
+#     'caption' => "What a great holiday party omg",
+#     'timestamp' => "11:37 PM December 31, 2019",
+#     'number_likes' => 13,
+#     'comments' => []
+#   }
+# ]
+#
+# puts posts
+# puts posts[0]
 
 
-posts = [
-  {
-    'image_src' => "./images/beach.png",
-    'caption' => "At the beach with my besties",
-    'timestamp' => "4:37 PM August 13, 2019",
-    'number_likes' => 0,
-    'comments' => []
-  }, 
-  {
-    'image_src' => "./images/holiday-party.png",
-    'caption' => "What a great holiday party omg",
-    'timestamp' => "11:37 PM December 31, 2019",
-    'number_likes' => 13,
-    'comments' => []
-  }
-]
-
-puts posts
-puts posts[0]
-
-
-# The code snippet above shows an Array with 2 elements. Each element in an 
-# Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in an
+# Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same
 # key-value pairs you used in your email Object above.
 # Then, log the email Array to the console.
--- section3/reflection.md
+++ section3/reflection.md
@@ -2,16 +2,50 @@
 
 1. What are two points from the Growth Mindset article and/or video that either resonated with you, or were brand new to you?
 
+I never thought about learning when to ask for help as being part of the growth mindset but it makes sense. Remaining stuck in one place because you are unwilling to ask for help or admit that you can't do it all yourself is not taking the opportunity to grow.
+
+This isn't entirely new to me, but I liked the reminder that continual practice is essential. As a writer, I always think that no writing is wasted, even if it ends up being deleted, because the learning from that writing stays in me. I think that it will be the same as I learned to code. All the effort I put in to learn will be stored in me, banked for later use, even if I can't see it.
+
 1. In which ways do you currently demonstrate a Growth Mindset? In which ways do you _not_?
+
+I am always trying to change my life and I don't really give much thought to whether I am capable of doing it. I just assume I am. Changing careers in my late 30s isn't easy and it became apparent to me when I resigned two weeks ago that it never occurred to some of my coworkers that they could simply walk away and do something else. They seemed really shocked that I would risk giving up my safe job to retrain for a better career.
+
+In one way I don't demonstrate a growth mindset and that is in asking for help when I need it. I always think that I can figure things out myself but I need to recognize that sometimes growth can't be achieved through individual efforts. It's funny because I don't expect this of other people or look down on people who ask for help, but for some reason I am harder on myself.
 
 1. What is a Hash, and how is it different from an Array?
 
+Hashes store data in un-indexed key-value pairs. Arrays store data in a ordered, integer-indexed list.
+
 1. In the space below, create a Hash stored to a variable named `pet_store`.  This hash should hold an inventory of items and the number of that item that you might find at a pet store.
+
+```ruby
+pet_store = {alfalfa_pellets: 12, hamster_wheels: 50, leashes: 7}
+```
 
 1. Given the following `states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}`, how would you access the value `"Iowa"`?
 
+```ruby
+puts states["IA"]
+```
+
 1. With the same hash above, how would we get all the keys?  How about all the values?
+
+```ruby
+# all the keys
+states.each do |abbrev, state|
+  puts abbrev
+end
+
+# all the values
+states.each do |abbrev, state|
+  puts state
+end
+```
 
 1. What is another example of when we might use a hash?  In your example, why is a hash better than an array?
 
+Hashes are more useful when storing paired data. So if you are going on a bowling field trip, a hash could store the name of each student paired with their shoe size. An array could only store the list of shoe sizes or the list of student names.
+
 1. What questions do you still have about hashes?
+
+I understand the difference between hashes and arrays I just can't fully understand how hashes are useful yet, but it's more of a general discomfort of trying to hold a new concept in my brain before I know what to do with it rather than specific questions.

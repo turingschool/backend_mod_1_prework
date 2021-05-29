1a40b55d7c93ef6af0e3e4c7a33277d40c9640d2
Weston Ellis <ellisweston112@gmail.com>
2021-05-29 15:56:16 -0600

    complete ex3.rb

---
+++ section1/ex3.rb
@@ -0,0 +1,25 @@
+puts "I will now count my chickens:"
+#25 plus (30 divided by 6)
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# 100 minus ((25 times 3) what is the remainer of divided by 4)
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+puts "Now I will count the eggs:"
+#exactly what it looks like
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+#see line 8, except the output is a true/false statement
+puts 3.0 + 2.0 < 5.0 - 7.0
+# the #{} is replaced by the function inside
+puts "What is 3 + 2? #{3.0 + 2.0}"
+#see line 14
+puts "what is 5 - 7? #{5.0 - 7.0}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+#see line 14, except the output is true/false
+puts "Is it greater #{5.0 > -2.0}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= -2.0}"

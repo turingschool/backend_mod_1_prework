c5164e954c9705d75ca82263817c7013c8ef9fb9
Isika Powers <isika.powers@gmail.com>
2021-05-29 14:28:56 -0600

    Refactor final_prep

--- final_prep/annotations.rb
+++ final_prep/annotations.rb
@@ -4,30 +4,38 @@
 
 # Build a Bear
 
-# This is a function with 5 parameters (name, age, fur, clothes, special_power)
+# This is a function with 5 parameters (name, age, fur, clothes, special_power).
 def build_a_bear(name, age, fur, clothes, special_power)
-  # Declare a variable of greeting - value is a string with the name parameter interpolated in
+  # Declare a variable of `greeting` - value is a string with the `name` parameter
+  # interpolated in.
   greeting = "Hey partner! My name is #{name} - will you be my friend?!"
-  # Declare a variable of demographics - value is a string with the name parameter and another value is an integer with the age parameter interpolated in
+  # Declare an array of demographics with 2 elements:
+  # 1st is a string with the name element.
+  # 2nd is an integer with the age element.
   demographics = [name, age]
-  # Declare a variable of power_saying - value is a string with the special_power parameter interpolated in
+  # Declare a variable of `power_saying` - value is a string with the `special_power`
+  # parameter interpolated in.
   power_saying = "Did you know that I can #{special_power}?"
-  # Declare a variable of built_bear - value is a hash of 6 key-value pairs with one of them ("saying") having an array value
+  # Declare a hash of built_bear with 6 key-value pairs.
   built_bear = {
-    'basic_info' => demographics,
-    'clothes' => clothes,
-    'exterior' => fur,
-    'cost' => 49.99,
+    'basic_info' => demographics, # The `basic_info` key has a value of the `demographics` array.
+    'clothes' => clothes, # The `clothes` key has a value of the `clothes` paremeter.
+    'exterior' => fur, # The `exterior` key has a value of the `fur` parameter.
+    'cost' => 49.99, # The `cost` key has a value of 49.99 (integer).
+    # The `sayings` key is an array with a value of 3 elements (`greeting` variable, `
+    # power_saying` variable, and "Goodnight my friend!" string ).
     'sayings' => [greeting, power_saying, "Goodnight my friend!"],
-    'is_cuddly' => true,
+    'is_cuddly' => true, # The `is_cuddly` key is a boolean, assigned a value to true.
   }
-  # When function is called, it will return the built_bear key-value pair values
+  # When function is called, it will return the built_bear hash.
   return built_bear
 end
 
-# Invoke the build_a_bear function with name = "Fluffy", age = 4, fur = "brown", clothes = ['pants', 'jorts', 'tanktop'], special_power = "give you nightmares"
+# Invoke the build_a_bear function with name = "Fluffy", age = 4, fur = "brown",
+# clothes = ['pants', 'jorts', 'tanktop'], special_power = "give you nightmares"
 build_a_bear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares')
-# Invoke the build_a_bear function with name = 'Sleepy', age = 2, fur = "purple", clothes = ['pajamas', 'sleeping cap'], special_power = "sleeping in"
+# Invoke the build_a_bear function with name = 'Sleepy', age = 2, fur = "purple",
+# clothes = ['pajamas', 'sleeping cap'], special_power = "sleeping in"
 build_a_bear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in')
 
 
@@ -35,18 +43,19 @@ build_a_bear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in')
 
 # This is a function with 3 parameters (num_1, num_2, range)
 def fizzbuzz(num_1, num_2, range)
-  # Declare variable i for each number in 1 to range parameter
-  # Iterate each variable i in 1 to range parameter
+  # Declare variable i for each number in 1 to `range` parameter
+  # Iterate each variable i in 1 to `range` parameter
   (1..range).each do |i|
-    # If variable i modolo parameter num_1 is equal to 0 and variable i modolo parameter num_2 is equal to 0
+    # If variable i is divisible by `num_1` parameter AND variable i is divisible
+    # by `num_2` parameter.
     if i % num_1 === 0 && i % num_2 === 0
       # Print "fizzbuzz"
       puts 'fizzbuzz'
-    # Else if variable i modulo parameter num_1 is equal to 0
+    # Else if variable i is divisible by `num_1` parameter.
     elsif i % num_1 === 0
       # Print "fizz"
       puts 'fizz'
-    # Else if variable i modolo paremeter num_2 is equal to 0
+    # Else if variable i is divisible by `num_2` paremeter.
     elsif i % num_2 === 0
       # Print "buzz"
       puts 'buzz'

b1c8cfb3a94422b93cfe1d39c92370c837da8212
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-05-29 15:41:33 -0600

    Add Section 1

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
@@ -11,49 +11,67 @@ p name
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+p house_elf
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+warning = "Harry Potter must not return to Hogwarts!"
+p warning
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
-# YOUR CODE HERE
-p students
+new_students = students + 2
+p new_students
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
-# YOUR CODE HERE
-p students
+departing_students = students -2
+p departing_students
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
+first_name = "Eric"
+
+is_hungry = "for tacos"
+
+number_of_pets = 2
 
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
+# How did you decide to use the data type you did for each of the three variables above?
 
 # Explain.
-
+# For first name, I chose my own first name and made it a string as names are rarely numbers.
+# Same logic for is_hungry. I want tacos, so I chose a string. Trying to keep the eventual
+# puts within the flow of a normal, human conversation
+# number_of_pets, the data type was a interger becuase I don't have a decimal value of dogs. Just 2.
 
 # YOU DO:
 # Re-assign the values to the three variables from the previous challenge to different values (but same data type).
 # print all three variables to the terminal.
 
+first_name = "Roberto"
+
+is_hungry = "loves"
+
+number_of_pets = 4
+
+puts "#{first_name} #{is_hungry} his #{number_of_pets} dogs with his whole heart."
 
 # YOU DO:
 # Using the variables below, print the total number of snacks to the terminal:
 healthy_snacks = 6;
 junk_food_snacks = 8;
 
+puts healthy_snacks + junk_food_snacks
+
 
 #-------------------
 # FINAL CHECK
 #-------------------
 
-# Did you run this file in your terminal to make sure everything printed out to the terminal 
-  # as you would expect? 
+# Did you run this file in your terminal to make sure everything printed out to the terminal
+  # as you would expect?

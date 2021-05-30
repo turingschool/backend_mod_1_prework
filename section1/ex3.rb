894d2643ee0dfee1de60471a1a82f0ad7ff13694
Michael Marchand <michaeldavidmarchand@gmail.com>
2021-05-30 17:56:53 -0600

    convert to floats, and correct modulo to modulus

--- section1/ex3.rb
+++ section1/ex3.rb
@@ -2,26 +2,26 @@
 puts "I will now count my chickens:"
 
 # output string with string interpolation
-puts "Hens #{25 + 30 / 6}"
-#output string with string interpolation and an example of modulo
-puts "Roosters #{100 - 25 * 3 % 4}"
+puts "Hens #{25.0 + 30 / 6}"
+#output string with string interpolation and an example of modulus
+puts "Roosters #{100.0 - 25 * 3 % 4}"
 
 # output string
 puts "Now I will count the eggs:"
 
 # complicated maths
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6.0
 
 # output string
 puts "Is it true that 3 + 2 < 5 - 7?"
 
 # evaluation of boolean operator <
-puts 3 + 2 < 5 - 7
+puts 3 + 2 < 5 - 7.0
 
 # output string
-puts "What is 3 + 2? #{3 + 2}"
+puts "What is 3 + 2? #{3 + 2.0}"
 #output string
-puts "What is 5 - 7? #{5 - 7}"
+puts "What is 5 - 7? #{5 - 7.0}"
 
 #output string
 puts "Oh, that's why it's false."
@@ -29,8 +29,8 @@ puts "Oh, that's why it's false."
 #output string
 puts "How about some more."
 # boolean evaluation inside string interpolation
-puts "Is it greater? #{5 > -2}"
+puts "Is it greater? #{5.0 > -2}"
 # more boolean inside string interpolation
-puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it greater or equal? #{5.0 >= -2}"
 # more boolean inside string interpolation
-puts "Is it less or equal? #{5 <= -2}"
+puts "Is it less or equal? #{5.0 <= -2}"

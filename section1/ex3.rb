c49067da83c3a640132fede68e42979b68e02844
Michael Marchand <michaeldavidmarchand@gmail.com>
2021-05-30 17:50:24 -0600

    add comments to ex3.rb

--- section1/ex3.rb
+++ section1/ex3.rb
@@ -1,23 +1,36 @@
+# output string
 puts "I will now count my chickens:"
 
+# output string with string interpolation
 puts "Hens #{25 + 30 / 6}"
+#output string with string interpolation and an example of modulo
 puts "Roosters #{100 - 25 * 3 % 4}"
 
+# output string
 puts "Now I will count the eggs:"
 
+# complicated maths
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# output string
 puts "Is it true that 3 + 2 < 5 - 7?"
 
+# evaluation of boolean operator <
 puts 3 + 2 < 5 - 7
 
+# output string
 puts "What is 3 + 2? #{3 + 2}"
+#output string
 puts "What is 5 - 7? #{5 - 7}"
 
+#output string
 puts "Oh, that's why it's false."
 
+#output string
 puts "How about some more."
-
+# boolean evaluation inside string interpolation
 puts "Is it greater? #{5 > -2}"
+# more boolean inside string interpolation
 puts "Is it greater or equal? #{5 >= -2}"
+# more boolean inside string interpolation
 puts "Is it less or equal? #{5 <= -2}"

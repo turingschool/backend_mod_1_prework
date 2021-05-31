96f330206cbfd0dde85bffaa5c07ee498b045470
Michael Marchand <michaeldavidmarchand@gmail.com>
2021-05-31 11:22:16 -0600

    add notes to ex7

--- section1/ex7.rb
+++ section1/ex7.rb
@@ -1,21 +1,39 @@
+# prints text
 puts "Mary had a little lamb."
+# prints text, but with string inside string interpolation...
 puts "Its fleece was white as #{'snow'}."
+# prints text
 puts "And everywhere that Mary went."
+# example of math being used on a `String` object
 puts "." * 10 # what'd that do?
 
+# assigns a string to a variable
 end1 = "C"
+# assigns a string to a variable
 end2 = "h"
+# assigns a string to a variable
 end3 = "e"
+# assigns a string to a variable
 end4 = "e"
+# assigns a string to a variable
 end5 = "s"
+# assigns a string to a variable
 end6 = "e"
+# assigns a string to a variable
 end7 = "B"
+# assigns a string to a variable
 end8 = "u"
+# assigns a string to a variable
 end9 = "r"
+# assigns a string to a variable
 end10 = "g"
+# assigns a string to a variable
 end11 = "e"
+# assigns a string to a variable
 end12 = "r"
 
 # watch that print vs. puts on this line what's it do?
+# `print` does not return to a newline
 print end1 + end2 + end3 + end4 + end5 + end6
+# `puts` does return to a newline (or, with a newline)
 puts end7 + end8 + end9 + end10 + end11 + end12

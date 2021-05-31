02092c59a75e3a6ccd042dda0eca20645a1d4384
Weston Ellis <ellisweston112@gmail.com>
2021-05-31 14:08:48 -0600

    Finished all tasks in section 1

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
@@ -15,27 +15,28 @@ house_elf = "Dobby"
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+obvious = "Harry Potter must not return to Hogwarts!"
+puts obvious
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
 # YOUR CODE HERE
-p students
+p students + 2
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
 # YOUR CODE HERE
-p students
+p students - 2
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
 
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
+# How did you decide to use the data type you did for each of the three variables above?
 
 # Explain.
 
@@ -55,5 +56,5 @@ junk_food_snacks = 8;
 # FINAL CHECK
 #-------------------
 
-# Did you run this file in your terminal to make sure everything printed out to the terminal 
-  # as you would expect? 
+# Did you run this file in your terminal to make sure everything printed out to the terminal
+  # as you would expect?
--- section1/reflection.md
+++ section1/reflection.md
@@ -2,18 +2,41 @@
 
 1. How did the SuperLearner Article resonate with you? What from this list do you already do? Want to start doing or do more of? Is there anything not on this list, that you would add to it?
 
-1. How would you print the string `"Hello World!"` to the terminal?
+I really enjoy the sentiment of learning as processual rather than being end oriented.  
+This tends to prevent a 'shutting off' of learning after a benchmark of understanding.  
+The thing that I need to do more of is eat better, gotta get them nutrients!
 
+1. How would you print the string `"Hello World!"` to the terminal?
+```ruby
+puts "Hello world"
+```
 1. What character is used to indicate comments in a ruby file?
+
+#
 
 1. Explain the difference between an integer and a float?
 
+integers cannot have a decimal place, floats have at least one.
+
 1. In the space below, create a variable `animal` that holds the string `"zebra"`
 
-1. How would you print the string `"zebra"` using the variable that you created above?
+animal = "zebra"
 
+1. How would you print the string `"zebra"` using the variable that you created above?
+```ruby
+puts animal
+```
 1. What is interpolation? Use interpolation to print a sentence using the variable `animal`.
 
+interpolation is slapping data inside of a string.
+```ruby
+puts "have you ever seen a #{animal}?"
+```
 1. What method is used to get input from a user?
-
+```ruby
+gets.chomp
+```
 1. Name and describe two common string methods:
+
+variable[0..3] :lists the characters in those positions.
+variable.split :gives an array and separates by a space (unless changed).

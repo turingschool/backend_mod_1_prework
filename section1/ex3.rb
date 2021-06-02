f1bdac45bd6ae07a2536ae013dfd9ba31e3de112
Andrew Patterson <andrew.patterson747@gmail.com>
2021-06-02 00:51:33 -0500

    Move Exercises from brother directory

---
+++ section1/ex3.rb
@@ -0,0 +1,41 @@
+# Make chicken counting statement
+puts "I will now count my chickens:"
+
+# Make total number of hens calculations
+puts "Hens #{25.0 + 30.0 / 6.0}"
+
+# Make total number of Roosters calculations
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# Make egg counting statement
+puts "Now I will count the eggs:"
+
+# counting eggs calculation
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# ask question whether line of math is true
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+# make calculation about previous question
+puts  3.0 + 2.0 < 5.0 - 7.0
+
+# ask question and do the math from previous math question
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+
+# ask question and do the math of the other side of that math question
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
+
+# have moment of realization
+puts "Oh, that's why its false."
+
+# "IS THAT ALL YOU'VE GOT???"
+puts "How about some more."
+
+# ask question, do math
+puts "Is it greater? #{5.0 > -2.0}"
+
+# ask another question, do more math
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+
+#ask yet another questioon, do some more math
+puts "Is it less or equal #{5.0 <= -2.0}"

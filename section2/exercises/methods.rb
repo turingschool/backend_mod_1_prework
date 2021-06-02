3db96812f77b905d22f14830e659b6b62cdd4c0a
Rteske <77249542+Rteske@users.noreply.github.com>
2021-06-02 15:46:09 -0700

    Complete Methods and Ifs

--- section2/exercises/methods.rb
+++ section2/exercises/methods.rb
@@ -11,22 +11,35 @@ end
 print_name
 
 # Write a method that takes a name as an argument and prints it:
+
 def print_name(name)
-  # YOUR CODE HERE
+  puts name
 end
 
 print_name("Albus Dumbledore")
 
-# Write a method that takes in 2 numbers as arguments and prints 
+# Write a method that takes in 2 numbers as arguments and prints
 # their sum. Then call your method three times with different arguments passed in:
 # YOUR CODE HERE
+def sum_of_2(x, y)
+  puts (x + y)
+end
 
-# Write a method that takes in two strings as arguments and prints 
-# a concatenation of those two strings. Example: The arguments could be 
-# (man, woman) and the end result might output: "When Harry Met Sally".  
-# Then call your method three times with different arguments passed in. 
+sum_of_2(2, 3)
+sum_of_2(4, 5)
+sum_of_2(9, 5)
 
+# Write a method that takes in two strings as arguments and prints
+# a concatenation of those two strings. Example: The arguments could be
+# (man, woman) and the end result might output: "When Harry Met Sally".
+# Then call your method three times with different arguments passed in.
+def string_add(left, right)
+  puts left + right
+end
 
+string_add("taco", "cat")
+string_add("tac", "ocat")
+string_add("dog", "gish")
 #-------------------
 # PART 3: Naming is Hard
 #-------------------
@@ -39,3 +52,8 @@ print_name("Albus Dumbledore")
 
 # EXPLAIN:
 
+=begin
+I named the first parameter 'name' because that is what we are printing in the console.
+The second I made 'x' and 'y' because its a math equation
+The third was becase if you entered the string in the wrong parameter then you could end up with a backwards saying instead of what was supposed to happen
+=end

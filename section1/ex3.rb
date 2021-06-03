a88b0c61c1fbf79631b360a5494f8cf90c90a982
Andrew Massey <acmassey@ncsu.edu>
2021-06-03 15:37:44 -0400

    Added DS Store and Completed Ruby Exercises 1 thru 4

---
+++ section1/ex3.rb
@@ -0,0 +1,33 @@
+# creates string "I will now count my chickens"
+puts "I will now count my chickens:"
+
+# creates formula counting both the hens and the roosters
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# creates string "Now I will count the eggs"
+puts "Now I will count the eggs:"
+
+# creates formula counting all of the eggs
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# puts the boolean in plain text string
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0"
+
+# tests the question in the line 15
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# solves for each side of the equation on line 15
+puts "What is 3 + 2? #{3.0 + 2.0}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+# creates string "oh, that's why it's false"
+puts "Oh, that's why it's false"
+
+# creates string "how about some more"
+puts "How about some more."
+
+# creates strings and following booleans for each of the three questions
+puts "Is it greater? #{5.0 > -2.0}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= -2.0}"

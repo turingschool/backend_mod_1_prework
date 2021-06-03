cea1bd236c0fc6c0571f808070c7d3d6c4fe110a
Stephanie Helm <stephaniemhelm@gmail.com>
2021-06-03 14:34:12 -0600

    Completed exercises

--- section2/exercises/methods.rb
+++ section2/exercises/methods.rb
@@ -10,21 +10,49 @@ end
 
 print_name
 
+def print_my_name
+  p "Stephanie Helm"
+end
+
+print_my_name
+
 # Write a method that takes a name as an argument and prints it:
 def print_name(name)
-  # YOUR CODE HERE
+  p name
 end
 
 print_name("Albus Dumbledore")
+print_name("Stephanie Helm")
 
-# Write a method that takes in 2 numbers as arguments and prints 
+
+
+# Write a method that takes in 2 numbers as arguments and prints
 # their sum. Then call your method three times with different arguments passed in:
 # YOUR CODE HERE
 
-# Write a method that takes in two strings as arguments and prints 
-# a concatenation of those two strings. Example: The arguments could be 
-# (man, woman) and the end result might output: "When Harry Met Sally".  
-# Then call your method three times with different arguments passed in. 
+def add(a, b)
+  puts a + b
+end
+
+add(4, 3)
+add(10, 11)
+add(1, 46)
+
+
+# Write a method that takes in two strings as arguments and prints
+# a concatenation of those two strings. Example: The arguments could be
+# (man, woman) and the end result might output: "When Harry Met Sally".
+# Then call your method three times with different arguments passed in.
+
+
+def favorite_beverage_list(name, favorite_beverage)
+  puts "#{name}'s favorite beverage is #{favorite_beverage}!"
+end
+
+favorite_beverage_list("Stephanie", "coffee")
+favorite_beverage_list("John", "beer")
+favorite_beverage_list("Mack", "juice")
+favorite_beverage_list("Sage", "tea")
 
 
 #-------------------
@@ -37,5 +65,4 @@ print_name("Albus Dumbledore")
 # Look at the code you wrote for the previous YOU DO🎈 - what did you name the function, and why?
 # What did you name each parameter, and why?
 
-# EXPLAIN:
-
+# EXPLAIN: I wanted to make a list of names and a list of their favorite beverage.  So I named the function favorite_beverage_list.  Then I named the parameters (name, favorite_beverage)!

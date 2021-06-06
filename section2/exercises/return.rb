6c0e8459edb7c97eabfbbcff0623db492e34ef47
Michael Marchand <michaeldavidmarchand@gmail.com>
2021-06-05 19:13:26 -0600

    Add return.rb and launch_school_exercises_for_methods.rb

---
+++ section2/exercises/return.rb
@@ -0,0 +1,15 @@
+def add_three(number)
+  number + 3
+end
+
+returned_value = add_three(4)
+puts returned_value
+
+
+def just_assignments(number)
+  foo = number + 3
+end
+
+add_three(5).times { puts 'this should print 8 times'}
+
+"hi there".length.to_s  

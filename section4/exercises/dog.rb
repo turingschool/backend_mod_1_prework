7d8216b1567b03f4a8ae18295c6dab93181edaa4
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-06-06 16:05:47 -0600

    Update person and dog exercises

--- section4/exercises/dog.rb
+++ section4/exercises/dog.rb
@@ -1,5 +1,5 @@
 # In the dog class below, write a `play` method that makes
-# the dog hungry. Call that method below the class, and 
+# the dog hungry. Call that method below the class, and
 # print the dog's hunger status.
 
 class Dog
@@ -9,6 +9,10 @@ class Dog
     @breed  = breed
     @name   = name
     @age    = age
+    @hungry = true
+  end
+
+  def play
     @hungry = true
   end
 
@@ -28,3 +32,5 @@ p fido.age
 p fido.hungry
 fido.eat
 p fido.hungry
+p fido.play
+fido.hungry 
--- section4/exercises/person.rb
+++ section4/exercises/person.rb
@@ -1,5 +1,37 @@
-# Create a person class with at least 2 attributes and 2 behaviors. 
+# Create a person class with at least 2 attributes and 2 behaviors.
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
-# YOUR CODE HERE
+class Person
+  attr_accessor :name, :age, :height
+
+  def initialize(name, age, height)
+    @name = name
+    @age = age
+    @height = height
+  end
+
+  def greeting
+    puts "Hello! My name is #{name}, I am #{age} years old, and stand #{height} feet tall!"
+  end
+
+  def info
+    puts "#{name} is #{age} years old and is #{height} feet tall."
+  end
+
+  def change_info(name, age, height)
+    self.name = name
+    self.age = age
+    self.height = height
+  end
+end
+
+
+eric = Person.new("Eric", "33", "6")
+blair = Person.new("Blair", "30", "6")
+
+puts eric.greeting
+puts blair.info
+eric.change_info("Eric", "35", "9")
+puts eric.info
+puts blair.info 

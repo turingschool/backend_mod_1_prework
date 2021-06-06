4b2d928c59ba76f759a152c38d586951f098c08e
Michael Marchand <michaeldavidmarchand@gmail.com>
2021-06-06 10:37:13 -0600

    Add ex34.rb to section3/exercises

---
+++ section3/exercises/ex34.rb
@@ -0,0 +1,29 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The animal at 1; the animal at index 1 is "ruby", the animal in the first position is "bear"
+# The third (3rd) animal; the animal at index 3 is "kangaroo", but the third animal in the list is "peacock"
+# The first (1st) animal; the animal at index 1 is "ruby", but the first animal in the list is "bear"
+# The animal at 3; The animal at index 3 is "kangaroo" but the third animal in the list is "peacock"
+# The fifth (5th) animal; The animal at index 5 is "playtpus", but the fith animal in the list is "whale"
+# The animal at 2; The animal at index 2 is "peacock", but the 2nd animal in the list is "ruby"
+# The sixth (6th) animal; The animal at index 6 does not exist, but the 6th animal in the list is "platypus"
+# The animal at 4; the animal at index 4 is "whale", but the 4th animal in the list is "kangaroo"
+
+#  Zed's instructions are awful in this section....like, his explanation is confusing to me; I don't think I've ever seen anyone use the terms
+# cardinal or ordinal when referring to the index of an Array or any collection in the tutorials; they always assume knowledge of a collection beginning at 0 for
+# the language being discussed, unless it's an outlier language that is indexed to 1.
+
+# at this point it's a reflex to know that Arrays are indexed to 0, and there are better resources out there to introduce new programmers to this idea, IMO
+
+# STUDY DRILLS
+# because day 0 is part of the new collection
+
+car_brands = %w(chevy ford porsche bugatti dodge honda acura lexus bmw mini)
+#chevy is at 0
+# ford is at 1
+# porsche is at 2, etc
+
+produce_veggies = %w(carrots broccoli turnips bokchoy celery beets greenbeans)
+# greenbeans is at 6
+#nothing is at 7
+# carrots are at 0, etc

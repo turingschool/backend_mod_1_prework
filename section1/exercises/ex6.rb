af4065c75bdbc1ceaef4a439e6ac5d23b2823bbf
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-06-08 13:54:52 -0600

    Move exercises to correct folder

---
+++ section1/exercises/ex6.rb
@@ -0,0 +1,37 @@
+# Assigning "types_of_people" the value of "10"
+types_of_people = 10
+# Assigning "x" the value of "There are #{types_of_people} types of people."
+x = "There are #{types_of_people} types of people."
+# Assigning "binary" the value " "binary" "
+binary = "binary"
+# Assigning "do_not" the value of " "don't" "
+do_not = "don't"
+# Assigning "y" the value of "" "those who know #{binary} and those who #{do_not}." "
+y = "those who know #{binary} and those who #{do_not}."
+
+# Prints the value assigned to varriable "x"
+puts x
+# Prints the value assigned to varriable "y"
+puts y
+
+# Prints " "I said "There are 10 types of people." "
+puts "I said: #{x}."
+# Prints " "I also said "those who know binary and those who don't. " "
+puts "I also said #{y}."
+
+# Assinging the value "false" to the variable "hilarious"
+hilarious = false
+# Assigning the value " "Isnt that joke so funny?! #{hilarious}" "
+# to the varriable "joke_evaluation".
+joke_evaluation = "Isnt that joke so funny?! #{hilarious}"
+
+# Prints the value of the variable "joke_evaluation"
+puts joke_evaluation
+
+# Assigns the value " "this is the left side of..." " to the variable "w"
+w = "this is the left side of..."
+# Assigns the value " "a string with a right side." " to the variable "e"
+e = "a string with a right side."
+
+# Prints the combined values of varriable "w" and "e", in that order.
+puts w + e

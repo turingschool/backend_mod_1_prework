422f5ab80f943919190087ad0e80a73f4dfa294f
Matthew Kimball <84795737+mekimball@users.noreply.github.com>
2021-06-08 17:50:40 -0600

    Update strings.rb

--- section1/exercises/strings.rb
+++ section1/exercises/strings.rb
@@ -4,12 +4,13 @@
 # `ruby section1/exercises/strings.rb`
 
 # Example: Write code that prints your name to the terminal:
-p "Alan Turing"
+prints "Matthew Kimball"
 
 # Write code that prints `Welcome to Turing!` to the terminal:
-p #YOUR CODE HERE
+prints "Welcome to Turing!"
 
 # Write code that prints `99 bottles of pop on the wall...` to the terminal:
-# YOUR CODE HERE
+prints "99 bottles of pop on the wall..."
 
 # Write out code to log one line from your favorite song or movie.
+prints "No pulse is no excuse not to dance."

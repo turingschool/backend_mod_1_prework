a04c760f18460fa37f683ccc8f11a806b1588ae6
Andrew Massey <acmassey@ncsu.edu>
2021-06-09 14:51:42 -0400

    Add work for section 4

--- section4/exercises/burrito.rb
+++ section4/exercises/burrito.rb
@@ -1,4 +1,4 @@
-# Add the following methods to this burrito class and 
+# Add the following methods to this burrito class and
 # call the methods below the class:
 # 1. add_topping
 # 2. remove_topping
@@ -6,14 +6,36 @@
 
 class Burrito
   attr_reader :protein, :base, :toppings
+
   def initialize(protein, base, toppings)
     @protein  = protein
     @base     = base
     @toppings = toppings
   end
+
+  def add_topping(topping)
+    puts "#{topping} was added to your burrito"
+  end
+
+  def remove_topping(topping)
+    puts "#{topping} was removed from your burrito"
+  end
+
+  def change_protein(protein)
+    @protein = protein
+    puts "#{protein} is now the protein in your burrito"
+  end
+
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
+p dinner.toppings
+
+dinner.add_topping('corn')
+dinner.remove_topping('corn')
+dinner.change_protein('chicken')
+
+p dinner.protein
 p dinner.toppings
--- section4/exercises/person.rb
+++ section4/exercises/person.rb
@@ -1,5 +1,28 @@
-# Create a person class with at least 2 attributes and 2 behaviors. 
+# Create a person class with at least 2 attributes and 2 behaviors.
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
+class Person
+attr_accessor :height, :weight, :age
 
-# YOUR CODE HERE
+  def initialize(height, weight, age)
+    @height = height
+    @weight = weight
+    @age = age
+  end
+
+  def hobbies(hobby)
+    p "One hobby of mine is #{hobby}"
+  end
+
+  def last_meal(entree, meal)
+    p "I had a #{entree} for #{meal}"
+  end
+end
+
+
+andrew = Person.new("5'10", "170 lbs", 23)
+puts andrew.height
+puts andrew.weight
+puts andrew.age
+andrew.hobbies('skateboarding')
+andrew.last_meal('Chipotle bowl', 'dinner')

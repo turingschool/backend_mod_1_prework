387b80d10207af5c99d46f50719ed33ffe8d52ae
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-06-10 08:58:29 -0600

    Add inheritance exercise

---
+++ section4/exercises/inheritance.rb
@@ -0,0 +1,16 @@
+class Animal
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+class GoodDog < Animal
+  def initialize(color)
+    super
+    @color = color
+  end
+end
+
+bruno = GoodDog.new("brown")

b68737fa8b059d6692b335fd7623234cf15ac124
Matthew Kimball <mekimba6@gmail.com>
2021-06-09 18:56:36 -0600

    add Section 1

--- section1/exercises/strings.rb
+++ section1/exercises/strings.rb
@@ -1,15 +1,20 @@
-# In the below exercises, write code that achieves
+# In the below exercises, write your code that achieves
 # the desired result. To check your work, run this
 # file by entering the following command in your terminal:
-# `ruby section1/exercises/strings.rb`
+# 'ruby section1/exercises/strings.rb'
 
 # Example: Write code that prints your name to the terminal:
-p "Alan Turing"
+p "Matthew Kimball"
 
-# Write code that prints `Welcome to Turing!` to the terminal:
-p #YOUR CODE HERE
+# Write code that prints 'Welcome to Turing!' to the terminal:
+p "Welcome to Turing!"
 
-# Write code that prints `99 bottles of pop on the wall...` to the terminal:
-# YOUR CODE HERE
+# Write code that prints '99 bottles of pop on the wall...' to the terminal:
+p "99 bottles of pop on the wall"
 
+<<<<<<< HEAD
+# Write out code to log on line of your favorite song or movie.
+p "No pulse is no excuse not to dance."
+=======
 # Write out code to log one line from your favorite song or movie.
+>>>>>>> c91157cb0c46e572838cbb1ecef8071d91a21146

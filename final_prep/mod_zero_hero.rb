5017822d91ac505b1bd4db58b197a53671cc08d9
Michael Marchand <michaeldavidmarchand@gmail.com>
2021-06-09 21:38:07 -0600

    Update mod_zero_hero

--- final_prep/mod_zero_hero.rb
+++ final_prep/mod_zero_hero.rb
@@ -1,40 +1,69 @@
 # Challenge - See if you can follow the instructions and complete the exercise in under 30 minutes!
 
 # Declare two variables - hero_name AND special_ability - set to strings
+hero_name = "captain obvious"
+special_ability = "well duh"
 
 # Declare two variables - greeting AND catchphrase
 #   greeting should be assigned to a string that uses interpolation to include the hero_name
 #   catchphrase should be assigned to a string that uses interpolation to include the special_ability
+greeting = "Hello! #{hero_name} at your service"
+catchphrase = "If you didn't know, my special ability is....#{special_ability}"
+
 
 # Declare two variables - power AND energy - set to integers
+power = 100
+energy = 100
 
 # Declare two variables - full_power AND full_energy
 #   full_power should multiply your current power by 500
 #   full_energy should add 150 to your current energy
+full_power = power * 500
+full_energy = energy + 150
 
 # Declare two variables - is_human and identity_concealed - assigned to booleans
-
+is_human = true
+identity_concealed = true
 
 # Declare two variables - arch_enemies AND sidekicks
 #   arch_enemies should be an array of at least 3 different enemy strings
 #   sidekicks should be an array of at least 3 different sidekick strings
+arch_enemies = ["Pat Obscurity", "Kelly Confusion", "Sym Bolism"]
+sidekicks = ["Huey", "Louie", "Dewey"]
 
 # Print the first sidekick to your terminal
+puts arch_enemies[0]
 
 # Print the last arch_enemy to the terminal
+puts arch_enemies[-1]
 
 # Write some code to add a new arch_enemy to the arch_enemies array
+def add_enemy(arr, bad_guy_str)
+  arr << bad_guy_str
+end
 
 # Print the arch_enemies array to terminal to ensure you added a new arch_enemey
+puts arch_enemies
 
 # Remove the first sidekick from the sidekicks array
+sidekicks.slice(0)
 
 # Print the sidekicks array to terminal to ensure you added a new sidekick
+puts sidekicks
 
 # Create a function called assess_situation that takes three arguments - danger_level, save_the_day, bad_excuse
 #   - danger_level should be an integer
-#   - save_the_day should be a string a hero would say once they save the day 
+#   - save_the_day should be a string a hero would say once they save the day
 #   - bad_excuse should be a string a hero would say if they are too afraid of the danger_level
+def assess_situation(danger_level = 0, save_the_day = "yay! We're safe!", bad_excuse = "All my nopes")
+  if danger_level > 50
+    puts bad_excuse
+  elsif danger_level >= 10 && danger_level <= 50
+    puts save_the_day
+  else
+    puts "Meh. Hard pass."
+  end
+end
 
 # Your function should include an if/else statement that meets the following criteria
 #   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the bad_excuse to the terminal
@@ -47,6 +76,7 @@ excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 # assess_situation(99, announcement, excuse) > Should print - 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 #assess_situation(21, announcement, excuse) > should print - 'Never fear, the Courageous Curly Bracket is here!'
 #assess_situation(3, announcement, excuse) > should print - "Meh. Hard pass."
+# all good in the 'hood
 
 # Declare a new variable - scary_monster - assigned to an hash with the following key/values
 #   - name (string)
@@ -55,17 +85,59 @@ excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 #   - citiesDestroyed (array)
 #   - luckyNumbers (array)
 #   - address (hash with following key/values: number , street , state, zip)
+scary_monster = {
+  "name" => "Sweetums",
+  "smell" => "Bad",
+  "weight" => 3,
+  "citiesDestroyed" => ["Salt Lake City", "Jacksonville", "Burlington"],
+  "luckyNumbers" => [7, 19, 22],
+  "address" => {
+    number: 1,
+    street: "Main",
+    state: "Anywhere",
+    zip: 60601
+  }
+}
 
 
 # Create a new class called SuperHero
 # - Your class should have the following DYNAMIC values
-#   - name 
+#   - name
 #   - super_power
-#   - age 
+#   - age
 # - Your class should have the following STATIC values
 #   - arch_nemesis, assigned to "The Syntax Error"
 #   - power_level = 100
-#   - energy_level = 50 
+#   - energy_level = 50
+class SuperHero
+  attr_accessor :name, :super_power, :age
+  @@arch_nemesis = "The Syntax Error"
+  @@power_level = 100
+  @@energy_level = 50
+
+  def initialize(name = "Capt. Binky", super_power = "Snuggling", age = 40)
+    @name = name
+    @super_power = super_power
+    @age = age
+  end
+
+  def say_name
+    puts @name
+  end
+
+  def maximize_energy
+    @@energy_level = 1000
+  end
+
+  def gain_power(num)
+    @@power_level += num
+  end
+
+  def getPowerLevel
+    puts @@power_level
+  end
+
+end
 
 # - Create the following class methods
 #   - say_name, should print the hero's name to the terminal
@@ -73,12 +145,15 @@ excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 #   - gain_power, should take an argument of a number and INCREASE the power_level by that number
 
 # - Create 2 instances of your SuperHero class
-
+default_hero = SuperHero.new
+steve = SuperHero.new("Steve", "Unloads a dishwasher with perfect efficiency", 100)
 
 # Reflection
 # What parts were most difficult about this exerise?
+# Some of the instructions were ambiguous; Without immediate feedback the end result will be my interpretation of the stories
 
 # What parts felt most comfortable to you?
+# filling in the ambiguity with my own intuition
 
 # What skills do you need to continue to practice before starting Mod 1?
-
+# I'd really like to get ahead, so i'm going to continue to practice building objects

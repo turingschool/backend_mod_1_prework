02e6ee280e127178465cbbf8f7701f159bdfd3c7
Kelsey Thompson <kelsthompson2@gmail.com>
2021-06-12 08:01:59 -0400

    Add final prep technical work

--- final_prep/annotations.rb
+++ final_prep/annotations.rb
@@ -3,19 +3,31 @@
 # Use the # to create a new comment
 
 # Build a Bear
-
+#defining a method called build_a_bear and assigning parameters of name, age, fur, clothes, special_power
 def build_a_bear(name, age, fur, clothes, special_power)
+#define a variable called greeting equal to a string
   greeting = "Hey partner! My name is #{name} - will you be my friend?!"
+#define a variable called demographics equal to an array
   demographics = [name, age]
+  #define a variable called power_saying equal to a string
   power_saying = "Did you know that I can #{special_power}?"
+
+#modeling a completed build a bear using a hash called built_bear with keys:  basic_info, clothes, exterior, cost, saying, is_cuddly.
   built_bear = {
+    #key basic_info is equal to the value demographics(variable from above)
     'basic_info' => demographics,
+    #key clothes is equal to a string or an array of strings
     'clothes' => clothes,
+    #key exterior is equal to the bear's fur (variable from above)
     'exterior' => fur,
+    #key cost is equal to the price in a float integer
     'cost' => 49.99,
+    #key sayings is equal to an array of bears greeting, power_saying, and a string
     'sayings' => [greeting, power_saying, "Goodnight my friend!"],
+    #key is_cuddly is equal to a boolean
     'is_cuddly' => true,
   }
+  #executes the compiled built_bear method
   return built_bear
 end
 
@@ -24,15 +36,20 @@ build_a_bear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in')
 
 
 # FizzBuzz
-
+#method called fizzbuzz with parameters equal to num_1, num_2, and range
 def fizzbuzz(num_1, num_2, range)
+  #does 'i' to each number from 1 to the number indicated by parameter range
   (1..range).each do |i|
+    #(lines 44-45) take the modulo i % num 1 AND modulo: i %num_2; if both equations result in 0, return/print to screen the string "fizzbuzz"
     if i % num_1 === 0 && i % num_2 === 0
       puts 'fizzbuzz'
+      #if the if statement is untrue, and only i modulo num_1 is equal to 0, then return and print to screen "fizz"
     elsif i % num_1 === 0
       puts 'fizz'
+      #if the if AND first elsif statements are untrue, and only i modulo num_2 is equal to 0, then return and print to screen "buzz"
     elsif i % num_2 === 0
       puts 'buzz'
+      #if neither num_1 nor num_2 return a modulo equal to 0, puts the original number passed through the method
     else
       puts i
     end
--- final_prep/mod_zero_hero.rb
+++ final_prep/mod_zero_hero.rb
@@ -1,40 +1,61 @@
 # Challenge - See if you can follow the instructions and complete the exercise in under 30 minutes!
 
 # Declare two variables - hero_name AND special_ability - set to strings
+hero_name = "Super Girl"
+special_ability = "outsmart anyone"
 
 # Declare two variables - greeting AND catchphrase
 #   greeting should be assigned to a string that uses interpolation to include the hero_name
 #   catchphrase should be assigned to a string that uses interpolation to include the special_ability
-
+greeting = "Hi, I'm #{hero_name}."
+catchphrase = "I am super in a way that I can #{special_ability}."
 # Declare two variables - power AND energy - set to integers
-
+power = 9
+energy = 98
 # Declare two variables - full_power AND full_energy
 #   full_power should multiply your current power by 500
 #   full_energy should add 150 to your current energy
+full_power = power * 10
+full_energy = energy + 150
 
 # Declare two variables - is_human and identity_concealed - assigned to booleans
-
+is_human = true
+identity_concealed = false
 
 # Declare two variables - arch_enemies AND sidekicks
 #   arch_enemies should be an array of at least 3 different enemy strings
 #   sidekicks should be an array of at least 3 different sidekick strings
-
+arch_enemies = ["Mr. Know-It-All", "The Physicist", "The Really Smart Girl"]
+sidekicks = ["Smarty 1", "Smarty 2", "Smarty 3"]
 # Print the first sidekick to your terminal
-
+puts sidekicks[0]
 # Print the last arch_enemy to the terminal
-
+puts arch_enemies[2]
 # Write some code to add a new arch_enemy to the arch_enemies array
-
+arch_enemies[3] = "Smart Guy"
 # Print the arch_enemies array to terminal to ensure you added a new arch_enemey
-
+puts arch_enemies
 # Remove the first sidekick from the sidekicks array
-
+sidekicks.delete("Smarty 1")
 # Print the sidekicks array to terminal to ensure you added a new sidekick
-
+puts sidekicks
 # Create a function called assess_situation that takes three arguments - danger_level, save_the_day, bad_excuse
 #   - danger_level should be an integer
-#   - save_the_day should be a string a hero would say once they save the day 
+#   - save_the_day should be a string a hero would say once they save the day
 #   - bad_excuse should be a string a hero would say if they are too afraid of the danger_level
+def assess_situation(danger_level, bad_excuse, save_the_day)
+
+  if danger_level > 50
+    puts bad_excuse
+  elsif danger_level > 10 && danger_level < 50
+    puts save_the_day
+  else
+    puts "Meh. Hard pass."
+  end
+end
+  puts assess_situation(51, "They're so smart. I'm too scared!", "I'm so smart. I knew I could save the day!")
+  puts assess_situation(33, "They're so smart. I'm too scared!", "I'm so smart. I knew I could save the day!")
+  puts assess_situation(8, "They're so smart. I'm too scared!", "I'm so smart. I knew I could save the day!")
 
 # Your function should include an if/else statement that meets the following criteria
 #   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the bad_excuse to the terminal
@@ -44,9 +65,10 @@
 #Test Cases
 announcement = 'Never fear, the Courageous Curly Bracket is here!'
 excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
-# assess_situation(99, announcement, excuse) > Should print - 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
-#assess_situation(21, announcement, excuse) > should print - 'Never fear, the Courageous Curly Bracket is here!'
-#assess_situation(3, announcement, excuse) > should print - "Meh. Hard pass."
+assess_situation(99, announcement, excuse) #> Should print - 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
+assess_situation(21, announcement, excuse) #> should print - 'Never fear, the Courageous Curly Bracket is here!'
+assess_situation(3, announcement, excuse) #> should print - "Meh. Hard pass."
+
 
 # Declare a new variable - scary_monster - assigned to an hash with the following key/values
 #   - name (string)
@@ -55,18 +77,54 @@ excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 #   - citiesDestroyed (array)
 #   - luckyNumbers (array)
 #   - address (hash with following key/values: number , street , state, zip)
-
+scary_monster = {
+  name: "Claws",
+  smell: "stinky",
+  weight: "400",
+  citiesDestroyed: ["Chicago", "Detroit", "SanDiego"],
+  luckyNumbers: [7, 15, 437],
+  address: {number: "9021", street: "University Ave", state: "Ohio", zip: "32907"}
+}
 
 # Create a new class called SuperHero
 # - Your class should have the following DYNAMIC values
-#   - name 
+#   - name
 #   - super_power
-#   - age 
+#   - age
 # - Your class should have the following STATIC values
 #   - arch_nemesis, assigned to "The Syntax Error"
 #   - power_level = 100
-#   - energy_level = 50 
+#   - energy_level = 50
+class SuperHero
+  attr_accessor :name, :super_power, :age
+  def initialize(name, super_power, age)
+    @name = name
+    @super_power = super_power
+    @age = age
+  end
 
+  @@arch_nemesis = "The Syntax Error"
+  @@power_level = 100
+  @@energy_level = 50
+
+
+  def say_name
+    puts "#{name}"
+  end
+
+  def maximize_energy
+    puts "#{@@energy_level * 20}"
+  end
+  def gain_power(number)
+    puts "#{@@power_level + number}"
+  end
+end
+
+super_coder = SuperHero.new("Super Coder", "Almost always codes perfectly", 37)
+super_pseudo = SuperHero.new("Super Pseudo", "Excellent Pseudocoding Skills", 27)
+puts super_coder.say_name
+puts super_coder.maximize_energy
+puts super_coder.gain_power(12)
 # - Create the following class methods
 #   - say_name, should print the hero's name to the terminal
 #   - maximize_energy, should update the energy_level to 1000
@@ -77,8 +135,8 @@ excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 
 # Reflection
 # What parts were most difficult about this exerise?
-
+#The class section was the most difficult for me.
 # What parts felt most comfortable to you?
-
+# Variables, conditional/if statemetns
 # What skills do you need to continue to practice before starting Mod 1?
-
+#Understanding classes (mostly defining variables, instances, etc.)

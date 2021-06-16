ce9ca1eb238c740283d722ad3232d63639d522bd
Gregory Fischer <gwhoisj@gmail.com>
2021-06-16 16:26:26 -0400

    Add section 3 work

--- section3/exercises/arrays.rb
+++ section3/exercises/arrays.rb
@@ -15,51 +15,56 @@
 # EXAMPLE: write code below that will print an array of animals.
 # Store the array in a variable.
 animals = ["Zebra", "Giraffe", "Elephant"];
-print animals
+p animals
 
 # EXAMPLE: Write code below that will print "Zebra" from the animals array
-# YOUR CODE HERE
-print animals[0]
+# YOUR CODE
+p animals[0]
 
 # YOU DO: Write code below that will print the number of elements in array of
 # animals from above.
-
+p animals.length()
 
 # YOU DO: Write code that will reassign the last item in the animals
 # array to "Gorilla"
-
+animals[2] = "Gorilla"
 
 # YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals[3] = "Lion"
 
 # YOU DO: Write code that will print the String "Elephant" in the animals array
-
+animals[2] = "Elephant"
+p animals[2]
 
 #-------------------
 # PART 2: Foods: Array Methods
 #-------------------
 
 # YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+pasta_types = ["spagetti", "tortalinni", "raviolli", "linguini"]
 
 # YOU DO: Write code below that will print the number of elements in the array of
 # foods from above.
-
+p pasta_types.length
 
 # YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 # print the changed array to verify "broccoli" has been added
-
+pasta_types.append("broccoli")
+p pasta_types
 
 # YOU DO: Write code below that removes the last item of food from the foods array and
 # print the changed array to verify that item has been removed
+pasta_types.pop
+p pasta_types
 
-
-# YOU DO: Write code to add 3 new foods to the array. 
+# YOU DO: Write code to add 3 new foods to the array.
   # There are several ways to do this - choose whichever you'd like!
 # Then, print the changed array to verify the new items have been added
-
+pasta_types = pasta_types + ["bread", "eggs", "milk"]
+p pasta_types
 # YOU DO: Remove the food that is in index position 0.
-
+pasta_types.delete_at(0)
+p pasta_types
 #-------------------
 # PART 3: Where are Arrays used?
 #-------------------
@@ -77,11 +82,11 @@ print animals[0]
 posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 # YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-# may be storing data? Come up with 3 examples - they could be from different web applications or 
+# may be storing data? Come up with 3 examples - they could be from different web applications or
 # all from the same one.
 
-# 1:
-# 2:
-# 3:
-
-
+# 1: An ordered collection of all of someone's tweets for example might be a good one, but I would imagine that they would
+# have to store the extra data somewhere (e.g. time of post, location if enabled, and device used if enabled)
+# 2: Playlists on music sites such as Spotify with links to the relevent song that can be called upon as necessary
+# 3: History of changed contact information maybe for sites like FB? I would be suprised if they didn't store that, and an
+# array might be a good option for that
--- section3/reflection.md
+++ section3/reflection.md
@@ -1,17 +1,31 @@
 ## Section 3 Reflection
 
 1. What are two points from the Growth Mindset article and/or video that either resonated with you, or were brand new to you?
-
+I found the idea of a Zone of Proximal Development to be an interesting idea that seems obvious, but that I'd never thouhght of before that development happens in places close to what we know, but just a little removed from our current knowledge. The other thing I found to be of particular interest is the S.M.A.R.T. goal system which seems to be much more structured than what I currently do.
 1. In which ways do you currently demonstrate a Growth Mindset? In which ways do you _not_?
+I think that by making my goals more specific I can really embody the ideas of having a Growth Mindset, at the moment I feel like although I have a defined direction, the actual goals that I've set aren't particularly set up to help me get further on my path. I feel what I do well though is understanding that mistakes are a necessary and ordinary part of everything including learning.
 
 1. What is a Hash, and how is it different from an Array?
-
+a Hash is a list, but it is unordered and instead of an index it uses keys that can be anything
 1. In the space below, create a Hash stored to a variable named `pet_store`.  This hash should hold an inventory of items and the number of that item that you might find at a pet store.
-
+```
+pet_store = {
+  "dog_treats" => 35,
+  "cat_treats" => 23,
+  "dog_food" => 126,
+  "cat_food" => 344
+}
+```
 1. Given the following `states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}`, how would you access the value `"Iowa"`?
+`states["IA"]`
 
 1. With the same hash above, how would we get all the keys?  How about all the values?
-
+```
+states.keys
+states.values
+```
 1. What is another example of when we might use a hash?  In your example, why is a hash better than an array?
+Using a hash to store more data about a particular thing in an array could be useful, for example for each dog treat you could store the name of the treat, the brand, the price, and the number on hand and have an array of those instead of doing it the basic way like I did above.
 
 1. What questions do you still have about hashes?
+The only thing I'm wondering is how efficient is it to put a Hash inside a Hash.

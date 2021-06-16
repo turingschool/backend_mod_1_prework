cc0fecf5d72c395dc1bce20b3ac80f3f248840eb
Gregory Fischer <gwhoisj@gmail.com>
2021-06-15 21:57:47 -0400

    Add examples

---
+++ section1/ex3.rb
@@ -0,0 +1,28 @@
+puts "I will now count my chickens:"
+
+#prints Hens + calculation in brackets
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+puts "Now I will count the eggs:"
+
+#prints just the calculation
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#prints boolean assessing validity of statement
+puts 3 + 2 < 5 - 7
+
+#same as the first
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+#prints the text followed by a boolean assessing validity of statment in brackets
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"

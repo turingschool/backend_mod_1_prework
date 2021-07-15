e42e69b75b90557ef1f5f756763ab86f665a9621
Sam Devine <samuel.will.devine@gmail.com>
2021-07-15 12:07:51 -0600

    Add Section3 work

--- section3/exercises/hashes.rb
+++ section3/exercises/hashes.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section3/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
@@ -8,52 +8,67 @@ foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+puts '-' * 10
+zoo = {lions: 2, monkeys: 4, seals: 6}
 p zoo
 
-# Write code that prints all of the 'keys' of the zoo variable 
+# Write code that prints all of the 'keys' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+puts '-' * 10
+p zoo.keys
 
-# Write code that prints all of the 'values' of the zoo variable 
+# Write code that prints all of the 'values' of the zoo variable
 # you created above:
-# YOUR CODE HERE
+puts '-' * 10
+p zoo.values
 
-# Write code that prints the value of the first animal of the zoo variable 
+# Write code that prints the value of the first animal of the zoo variable
 # you created above:
-# YOUR CODE HERE
+puts '-' * 10
+p zoo[:lions]
 
-# Write code that adds an animal to the zoo hash. 
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
-
+puts '-' * 10
+zoo[:bears] = 3
+p zoo
 
 #-------------------
 # Part 2: Email
 #-------------------
-
+puts ''
+print '-' * 5
+print 'Part 2: Email'
+puts '-' * 5
 
 # Think about all the pieces of information associated with one single email in your inbox.
 # It has a sender, a subject, ...
 
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
+email = {'subject' => "A message from a Nigerian Prince", 'sender' => "spambot3000", 'cc' => ['radgranny@yahoo.com', 'old_timer42@gmail.com'], 'bcc' => ['boss@scammers.biz','nigerianking@scammers.biz'], 'body' => "Help me please! I have been captured. Send 0.2 Bitcoin to this address and my father, the king, will reward you handsomely: xia0gojw0ae[oefhijae;wffdas"}
 
 # Write code that prints your email hash to the terminal.
 
+puts email
 
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
-# YOUR CODE HERE
+puts '-' * 10
+puts email.keys
 
-# Write code that prints all of the 'values' of the email hash 
+# Write code that prints all of the 'values' of the email hash
 # you created above:
-# YOUR CODE HERE
-
+puts '-' * 10
+puts email.values
 
 #-------------------
 # Part 3: Many Emails - OPTIONAL EXTENSION
 #-------------------
+puts ''
+print '-' * 5
+print 'Part 3: Many Emails'
+puts '-' * 5
 
 # LONG EXAMPLE:
 # Now that we've learned about Objects AND Arrays, we can combine them.
@@ -64,7 +79,7 @@ p zoo
 # posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 
-# Frankly, that was a very simplified version of the Array the Instagram developers have 
+# Frankly, that was a very simplified version of the Array the Instagram developers have
 # written and work with. Still probably slightly simplified as we don't know what their code
 # actually looks like, but it may look more like this:
 
@@ -76,7 +91,7 @@ posts = [
     'timestamp' => "4:37 PM August 13, 2019",
     'number_likes' => 0,
     'comments' => []
-  }, 
+  },
   {
     'image_src' => "./images/holiday-party.png",
     'caption' => "What a great holiday party omg",
@@ -90,12 +105,39 @@ puts posts
 puts posts[0]
 
 
-# The code snippet above shows an Array with 2 elements. Each element in an 
-# Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in an
+# Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same
 # key-value pairs you used in your email Object above.
 # Then, log the email Array to the console.
+
+inbox = [
+  {
+    'sender' => 'marketing@bestbuy.com',
+    'subject' => 'Check out these CRAZY summer deals!!!!!!!!!',
+    'cc' => [],
+    'bcc' => ['george@gmail.com, harry@aol.com, edith@yahoo.com'],
+    'body' => 'Please come into the store and buy things from us! We are DESPERATE for you to spend your money here!! We really care about you and your needs so we are offering you a giant TV for only $999.99! HURRY this deal ends at midnight!',
+  },
+  {
+    'sender' => 'mom@hotmail.com',
+    'subject' => 'Cookie Recipe',
+    'cc' => ['brother@gmail.com', 'sister@gmail.com'],
+    'bcc' => [],
+    'body' => 'Here is the recipe you all asked for! Ingredients: 2.25 cups flour, 1 teaspoon baking soda, 0.5 teaspoons salt, 1 cup butter, 1.5 cups sugar, 1 egg, 1 teaspoon vanilla, 2 cups chocolate chips. Mix together and bake at 350 for 10 minutes. Tasty!',
+  },
+  {
+    'sender' => 'marketing@thespotgym.com',
+    'subject' => 'New Programs for July!',
+    'cc' => [],
+    'bcc' => ['climberboi94@gmail.com', 'rockclimber17@yahoo.com'],
+    'body' => 'We are open again, come climb with us! Click the link in the email to sign up for our competition at the end of the month!',
+  }
+]
+
+puts ''
+puts inbox
--- section3/reflection.md
+++ section3/reflection.md
@@ -2,16 +2,62 @@
 
 1. What are two points from the Growth Mindset article and/or video that either resonated with you, or were brand new to you?
 
+I found it really interesting that the article mentioned SMART Goals and Zones of Proximal Development; these are both terms that I heard occasionally in my most recent job but initially dismissed as corporate jargon. The article however described each idea succicntly and I think they make a lot of sense.
+
+I also loved hearing Carmen Popoviciu talk about measuring your success with your own progress, instead of comparing yourself to others. This concepts blends really well with a growth mindset because it can help you regardless of where you stand in comparison to others (if you're behind, measuring your own progress keeps you from becoming discouraged. If you're ahead, measuring your own progress prevents you from becoming complacent).
+
 1. In which ways do you currently demonstrate a Growth Mindset? In which ways do you _not_?
+
+I understand that there's always room to grow! The more I learn about a subject, the more I begin to realize how much I don't know. This is super motivating to me and can help me learn much more than I would have if I simply assumed that I already learned all I need to know.
+
+Something that I struggle with on some level is being open to feedback. Giving good feedback is a skill, but so is being open to and accepting feedback! Identifying feedback I receive as a learning opportunity rather than someone pointing out that I'm wrong is definitely an area where I can improve!
 
 1. What is a Hash, and how is it different from an Array?
 
+A Hash is similar to an array, in that both data types are comprised of a list of data. An array is an _ordered_ list, and you can use indexes to access data in the array. `Array[0]` will return the first object in the array, `Array[1]` will return the second object, and so on. A **hash** is different, however. Lists of data within a hash are not ordered based on an index, but based instead on **key-value pairs**. Hashes are defined with a key pertaining to each value, and that key acts like an address to find the value, the same way that an index works as an address in an array. For example, a hash might contain the names of states as the values, and use those states' abbreviations as keys. `states['CA']` in this instance should return `'California'`.
+
 1. In the space below, create a Hash stored to a variable named `pet_store`.  This hash should hold an inventory of items and the number of that item that you might find at a pet store.
+
+```ruby
+pet_store = {'dog_toys' => 128, 'cat_toys' => 51, 'fish_food' => 492, 'mice' => 45, 'dog_crates' => 12}
+```
 
 1. Given the following `states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}`, how would you access the value `"Iowa"`?
 
+`states["IA"]`
+
 1. With the same hash above, how would we get all the keys?  How about all the values?
+
+```ruby
+states.keys
+states.values
+```
 
 1. What is another example of when we might use a hash?  In your example, why is a hash better than an array?
 
+We could use a hash to store data about Instagram posts like in the example in `hashes.rb`. The given code provides an example of what kinds of data might be stored in an _array of hashes_ representing a given user's posts, with each hash in the array pointing to a single post:
+
+```ruby
+posts = [
+  {
+    'image_src' => "./images/beach.png",
+    'caption' => "At the beach with my besties",
+    'timestamp' => "4:37 PM August 13, 2019",
+    'number_likes' => 0,
+    'comments' => []
+  },
+  {
+    'image_src' => "./images/holiday-party.png",
+    'caption' => "What a great holiday party omg",
+    'timestamp' => "11:37 PM December 31, 2019",
+    'number_likes' => 13,
+    'comments' => []
+  }
+]
+```
+
+In this example, a hash is used for each post instead of an array, because it allows us to easily access each piece of information as long as we know what it's called. We don't need to refer to a key somewhere that tells us `caption` is always in the second index of each array, `number_likes` is always in the fourth spot, and so on. We also don't have to worry about the data being out of order somehow (if `image_src` ended up at the end of the array instead of the beginning, it could cause trouble when we're looking for it to display a thumbnail on a different page, etc.).
+
 1. What questions do you still have about hashes?
+
+How many objects can be stored in a hash? Is it infinite? Does it depend on your computer's capability?

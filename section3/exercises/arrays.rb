bd9a5b0a01431448d905effc4df2620528cd5038
Justina Hortizuela <justinahortizuela@gmail.com>
2021-07-17 10:18:13 -0700

    Add Changes

--- section3/exercises/arrays.rb
+++ section3/exercises/arrays.rb
@@ -1,4 +1,4 @@
-test
+
 # In the exercises below, write your own code where indicated
 # to achieve the desired result.
 
@@ -23,16 +23,25 @@ print animals[0]
 
 # YOU DO: Write code below that will print the number of elements in array of
 # animals from above.
+ animals.count
+
+
 
 
 # YOU DO: Write code that will reassign the last item in the animals
 # array to "Gorilla"
 
+animals << "Gorilla"
 
 # YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals.insert(3, "Cat")
+p animals
 
 # YOU DO: Write code that will print the String "Elephant" in the animals array
+p animals[2]
+
+
+
 
 
 #-------------------
@@ -40,26 +49,36 @@ print animals[0]
 #-------------------
 
 # YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+groceries = ["milk", "eggs", "ham", "bread"]
 
 # YOU DO: Write code below that will print the number of elements in the array of
 # foods from above.
+  groceries.count
+
 
 
 # YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 # print the changed array to verify "broccoli" has been added
+ groceries.unshift("broccoli")
+
+
 
 
 # YOU DO: Write code below that removes the last item of food from the foods array and
 # print the changed array to verify that item has been removed
+groceries.pop
+p groceries
 
 
 # YOU DO: Write code to add 3 new foods to the array.
   # There are several ways to do this - choose whichever you'd like!
 # Then, print the changed array to verify the new items have been added
+groceries.insert(3, "soda", "wine", "juice")
+p groceries
 
 # YOU DO: Remove the food that is in index position 0.
-
+ groceries.delete_at(0)
+p groceries
 #-------------------
 # PART 3: Where are Arrays used?
 #-------------------
@@ -80,6 +99,6 @@ posts = ["image at beach", "holiday party", "adorable puppy", "video of cute bab
 # may be storing data? Come up with 3 examples - they could be from different web applications or
 # all from the same one.
 
-# 1:
-# 2:
-# 3:
+# 1: Facebook. I see lists being utilized where arrays are storing data in arrays is personalized_ads = ["dog collars", "wonder bra", "new shoes"]
+# 2: Facebook. I also see arrays in memories_posts = ["post from 2009", "post from 2012", "post from 2015"]
+# 3: facebook. I see arrays in different groups you follow. groups = ["dog group", "runners group", "Harry potter fans group"]
